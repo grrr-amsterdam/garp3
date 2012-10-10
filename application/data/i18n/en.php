9041cf5b6b4c4b72d994a28ba986a8d5b07e4e60
Harmen Janssen <whatstyle@gmail.com>
2012-10-10 12:34:58 +0200

    Delete ALL THE THINGS. Clean slate mode activated.

--- application/data/i18n/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * en_Us translation file.
- * English is considered the source language for all system messages, so this 
- * file is only here for consistency.
- */
-$translationTable = array(
-);
