fecc82f8002d5afbfd76e715c96aa7f41bfcef33
Harmen Janssen <whatstyle@gmail.com>
2012-10-22 12:31:33 +0200

    Garp Git interface

--- library/Garp/Cli/Command/Git.php
+++ library/Garp/Cli/Command/Git.php
@@ -16,11 +16,92 @@ class Garp_Cli_Command_Git extends Garp_Cli_Command {
  	 * @param Array $args No arguments required, passing some will result in error.
  	 * @return Void
  	 */
-	public function pull($args = array()) {
+	public function pull(array $args = array()) {
 		if (!empty($args)) {
 			Garp_Cli::errorOut('Invalid option: '.$args[0]);
 			return false;
 		}
 		passthru('git pull --recurse-submodules && git submodule foreach git pull');
+	}
+
+
+	/**
+ 	 * Commit a submodule
+ 	 * @param Array $args
+ 	 * @return Void
+ 	 */
+	public function commitSubmodule(array $args = array()) {
+		if (empty($args[0])) {
+			Garp_Cli::errorOut('Specify submodule as first argument.');
+			return false;
+		}
+		if (empty($args['m'])) {
+			Garp_Cli::errorOut('Aborting commit due to empty commit message');
+			return false;
+		}
+		$submodule = $args[0];
+		$submodule = rtrim($submodule, DIRECTORY_SEPARATOR);
+		$commitMessage = $args['m'];
+
+		$projectRoot = getcwd();
+
+		// 1) Change into submodule
+		chdir($submodule);
+
+		// 2) Read status and organise index
+		$gitStatus = `git status --porcelain`;
+		$gitStatus = explode("\n", $gitStatus);
+		foreach ($gitStatus as $statusLine) {
+			if (!$statusLine) {
+				continue;
+			}
+			$statusCodeIndex = $statusLine[0];
+			$statusCodeWorkingCopy = $statusLine[1];
+			$path = substr($statusLine, 3);
+
+			// Status code checks
+			$modifiedButNotAdded = $statusCodeWorkingCopy == 'M';
+			$removedFile = $statusCodeWorkingCopy == 'D' && $statusCodeIndex == ' ';
+			$untrackedFile = $statusCodeIndex == '?' && $statusCodeWorkingCopy == '?';
+			if ($modifiedButNotAdded) {
+				passthru('git add '.$path);
+			} elseif ($removedFile) {
+				$removeFile = Garp_Cli::prompt('Do you want to remove file '.$path.'? Yn');
+				if (!$removeFile || strtolower($removeFile) == 'y') {
+					passthru('git rm '.$path);
+				}
+			} elseif ($untrackedFile) {
+				$addFile = Garp_Cli::prompt('Do you want to add untracked file '.$path.'? Yn');
+				if (!$addFile || strtolower($addFile) == 'y') {
+					passthru('git add '.$path);
+				}
+			}
+		}
+
+		// 3) Commit and push!
+		passthru('git commit -m \''.$commitMessage.'\'');
+		passthru('git push origin master');
+
+		// 4) Change back to project root
+		chdir($projectRoot);
+
+		// 5) Add and commit the submodule
+		passthru('git add '.$submodule);
+		$submoduleCommitMessage = 'Updated '.basename($submodule);
+		passthru('git commit -m \''.$submoduleCommitMessage.'\'');
+	}
+
+
+	/**
+ 	 * Help
+ 	 */
+	public function help() {
+		Garp_Cli::lineOut('Usage:');
+		Garp_Cli::lineOut('Pull from remote and update submodules');
+		Garp_Cli::lineOut('  g Git pull');
+		Garp_Cli::lineOut('');
+		Garp_Cli::lineOut('Commit a submodule');
+		Garp_Cli::lineOut('  g Git commitSubmodule garp --m=\'<your commit message>\'');
+		Garp_Cli::lineOut('');
 	}
 }
