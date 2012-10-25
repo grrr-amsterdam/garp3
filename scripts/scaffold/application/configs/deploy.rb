0d74b12819e663e90833b2ac9b614b35a0b7559b
David Spreekmeester <david@grrr.nl>
2012-10-25 10:29:35 +0200

    scaffold deploy

---
+++ scripts/scaffold/application/configs/deploy.rb
@@ -0,0 +1,25 @@
+# set :application, "MY_CUTE_APP_NAME"
+# set :repository,  "git@flow.grrr.nl:MY_LOVELY_REPOSITORY"
+# 
+# 
+# 
+# task :integration do
+#   server "MY_SEXY_SSH_HOST", :app, :web, :db, :primary => true
+#   set :deploy_to, "/EXAMPLE/EXAMPLE/public"
+#   set :user, "MY_BEDAZZLING_SSH_USERNAME"  
+#   set :garp_env, "integration"
+# end
+# 
+# task :staging do
+#   server "MY_SEXY_SSH_HOST", :app, :web, :db, :primary => true
+#   set :deploy_to, "/EXAMPLE/EXAMPLE/web"
+#   set :user, "MY_BEDAZZLING_SSH_USERNAME"
+#   set :garp_env, "staging"
+# end
+# 
+# task :production do
+#   server "MY_SEXY_SSH_HOST", :app, :web, :db, :primary => true
+#   set :deploy_to, "/EXAMPLE/EXAMPLE/public"
+#   set :user, "MY_BEDAZZLING_SSH_USERNAME"
+#   set :garp_env, "production"
+# end
