052fbd45f6df23771c75bb07da5864c75f3b21cd
Harmen Janssen <whatstyle@gmail.com>
2012-11-15 13:53:05 +0100

    Make sure Version command is executed from within Git repo

--- library/Garp/Cli/Command/Version.php
+++ library/Garp/Cli/Command/Version.php
@@ -14,7 +14,16 @@ class Garp_Cli_Command_Version extends Garp_Cli_Command {
  	 */
 	public function update(array $args = array()) {
 		$versionFilePath = APPLICATION_PATH.'/configs/version.php';
+		// Make sure we're in the project root before executing git rev-parse
+		$oldWorkingDir = getcwd();
+		chdir(APPLICATION_PATH.'/..');
+
+		// Fetch hash of HEAD revision
 		$headGitHash = `git rev-parse --verify --short HEAD`; //  2> /dev/null
+
+		// Change back into original dir
+		chdir($oldWorkingDir);
+
 		// This might be the case when working with an exported repo
 		// @todo Think about how to handle that
 		if (is_null($headGitHash)) {
