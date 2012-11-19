8dac2c7720cccfc9cd9731efb67e6431662ead82
Harmen Janssen <whatstyle@gmail.com>
2012-11-19 18:22:06 +0100

    Moved the Garp_Util_Loader namespace to Garp_Loader

---
+++ library/Garp/Loader/PluginLoader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Garp_Loader_PluginLoader
+ * Overwrites Zend_Loader_PluginLoader for more intelligent pluginLoaderCache handling.
+ *
+ * @author       Harmen Janssen | grrr.nl
+ * @package      Garp
+ * @subpackage   Loader
+ */
+class Garp_Loader_PluginLoader extends Zend_Loader_PluginLoader {
+}
