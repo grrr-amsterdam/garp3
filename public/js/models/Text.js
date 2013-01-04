c4f103c4231044eba03ffb03a73e492c281c5abe
Peter Schilleman <peter@grrr.nl>
2013-01-04 14:51:26 +0100

    Title/name support added for text wysiwyg

--- public/js/models/Text.js
+++ public/js/models/Text.js
@@ -3,14 +3,15 @@ Garp.dataTypes.Text.on('init', function(){
 	
 	this.iconCls = 'icon-text';
 	
-	//this.extraTypes = [['',__('Normal')],['aside',__('Aside')],['attention',__('Attention')]];
-	
 	// Wysiwyg Editor
 	this.Wysiwyg = Ext.extend(Garp.WysiwygAbstract, {
 		
 		allowedTags: ['a','b','i','br','p','div','ul','ol','li'],
 		
-		data: null,
+		data: {
+			description: null,
+			name: null
+		},
 		
 		filterHtml: function(){
 			var scope = this;
@@ -45,16 +46,39 @@ Garp.dataTypes.Text.on('init', function(){
 		getData: function(){
 			if (this.contentEditableEl) {
 				return {
-					description: this.contentEditableEl.dom.innerHTML
+					description: this.contentEditableEl.dom.innerHTML,
+					name: this.data.name || false
 				};
 			} else {
 				return '';
 			}
 		},
 		
+		setTitle: function(text){
+			this.data.name = text;
+			this.titleEl.update(text);
+			this.titleEl.setDisplayed( text ? true : false);
+		},
+		
+		showTitleDialog: function(){
+			Ext.Msg.prompt(__('Garp'), __('Please specify a title or leave empty to remove title:'), function(btn, text){
+				if (btn == 'ok') {
+					this.setTitle(text);
+				}
+			}, this, 80, this.data.name);
+		},
+		
+		getMenuOptions: function(){
+			return [{
+				group: '',
+				text: 'Add / remove title',
+				handler: this.showTitleDialog
+			}];
+		},
+		
 		initComponent: function(){
 
-			this.html += '<div class="contenteditable">' +
+			this.html += '<h4 class="contenttitle"></h4>'+'<div class="contenteditable">' +
 				 			__('Enter text') +
 						'</div>'; 
 		
@@ -69,13 +93,20 @@ Garp.dataTypes.Text.on('init', function(){
 
 				this.contentEditableEl = this.el.child('.contenteditable');
 				this.contentEditableEl.dom.setAttribute('contenteditable', true);
+				this.contentEditableEl.removeAllListeners();
 				this.contentEditableEl.on('focus', this.filterHtml, this);
 				this.contentEditableEl.on('click', this.filterHtml, this);
 				this.contentEditableEl.on('blur', this.filterHtml, this);
 				
+				this.titleEl = this.el.child('.contenttitle');
+				this.titleEl.removeAllListeners();
+				this.titleEl.on('click', this.showTitleDialog, this);
+				
 				if (this.data) {
 					this.contentEditableEl.update(this.data.description);
+					this.titleEl.update(this.data.name || '');
 				}
+				this.titleEl.setDisplayed( (this.data && this.data.name) || false);
 			}, this);
 			
 			Garp.dataTypes.Text.Wysiwyg.superclass.initComponent.call(this, arguments);
