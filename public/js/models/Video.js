4807ee707da5bba374094272cc4c5339d4dc9a92
Peter Schilleman <peter@grrr.nl>
2013-01-23 11:16:05 +0100

    Ticket #882: Video borkt als Image datatype er niet is

--- public/js/models/Video.js
+++ public/js/models/Video.js
@@ -86,83 +86,82 @@ Garp.dataTypes.Video.on('init', function(){
 		}
 	});
 	
-
-	this.Wysiwyg = Ext.extend(Garp.dataTypes.Image.Wysiwyg, {
+	if (Garp.dataTypes.Image && Garp.dataTypes.Image.Wysiwyg) {
+		this.Wysiwyg = Ext.extend(Garp.dataTypes.Image.Wysiwyg, {
 		
-		model: 'Video',
-		idProperty: 'id',
-		
-		pickerHandler: function(sel, afterInitCb){
-		
-			this._data = {
-				id: sel.data.id,
-				image: sel.data.image
-			};
-			var args = Array.prototype.slice.call(arguments);
-			args.shift();
-			afterInitCb.call(this, args);
-		},
-		
-		getData: function(){
-			return {
-				id: this._data.id,
-				image: this._data.image
-			};
-		},
-		
-		initComponent: function(){
+			model: 'Video',
+			idProperty: 'id',
 			
-			this.html += '<div class="contenteditable"></div>'; 
-		
-			this.addClass('wysiwyg-image');
-			this.addClass('wysiwyg-box');
-			if (this.col) {
-				this.addClass(this.col);
-			}
+			pickerHandler: function(sel, afterInitCb){
 			
-			this.on('user-resize', function(w, nw){
-				this.setHeight(this.resizeContent(nw));
-			});
-			this.on('afterrender', function(){
-				this.contentEditableEl = this.el.child('.contenteditable');
-				this.contentEditableEl.update('');
-				this.contentEditableEl.dom.setAttribute('contenteditable', false);
-				
-				var i = new Image();
-				var scope = this;
-				var path = this._data.image;
-				i.onload = function(){
-					Ext.apply(scope._data, {
-						width: i.width,
-						height: i.height
-					});
-					
-					scope.contentEditableEl.setStyle({
-						position: 'relative',
-						padding: 0
-					});
-					
-					scope.contentEditableEl.update('<div class="img"></div>');
-					scope.contentEditableEl.child('.img').setStyle({
-						backgroundImage: 'url("' + path + '")'
-					});
-					
-					scope.resizeContent(scope.contentEditableEl.getWidth());
-					if (scope.ownerCt) {
-						scope.ownerCt.doLayout();
-					}
-					
+				this._data = {
+					id: sel.data.id,
+					image: sel.data.image
 				};
-				i.src = path;
-				if (i.complete) {
-					i.onload();
+				var args = Array.prototype.slice.call(arguments);
+				args.shift();
+				afterInitCb.call(this, args);
+			},
+			
+			getData: function(){
+				return {
+					id: this._data.id,
+					image: this._data.image
+				};
+			},
+			
+			initComponent: function(){
+			
+				this.html += '<div class="contenteditable"></div>';
+				
+				this.addClass('wysiwyg-image');
+				this.addClass('wysiwyg-box');
+				if (this.col) {
+					this.addClass(this.col);
 				}
-			}, this);
-			
-			Garp.dataTypes.Image.Wysiwyg.superclass.initComponent.call(this, arguments);
-			
-		}
-		
-	});
-
+				
+				this.on('user-resize', function(w, nw){
+					this.setHeight(this.resizeContent(nw));
+				});
+				this.on('afterrender', function(){
+					this.contentEditableEl = this.el.child('.contenteditable');
+					this.contentEditableEl.update('');
+					this.contentEditableEl.dom.setAttribute('contenteditable', false);
+					
+					var i = new Image();
+					var scope = this;
+					var path = this._data.image;
+					i.onload = function(){
+						Ext.apply(scope._data, {
+							width: i.width,
+							height: i.height
+						});
+						
+						scope.contentEditableEl.setStyle({
+							position: 'relative',
+							padding: 0
+						});
+						
+						scope.contentEditableEl.update('<div class="img"></div>');
+						scope.contentEditableEl.child('.img').setStyle({
+							backgroundImage: 'url("' + path + '")'
+						});
+						
+						scope.resizeContent(scope.contentEditableEl.getWidth());
+						if (scope.ownerCt) {
+							scope.ownerCt.doLayout();
+						}
+						
+					};
+					i.src = path;
+					if (i.complete) {
+						i.onload();
+					}
+				}, this);
+				
+				Garp.dataTypes.Image.Wysiwyg.superclass.initComponent.call(this, arguments);
+				
+			}
+		});
+	}
 });
