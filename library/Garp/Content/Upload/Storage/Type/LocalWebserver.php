1ca865b4012a4bcdb790a06b791372632b1433b5
David Spreekmeester <david@grrr.nl>
2013-02-22 17:56:36 +0100

    Upload syncer

---
+++ library/Garp/Content/Upload/Storage/Type/LocalWebserver.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Garp_Content_Upload_FileList_LocalWebserver
+ * 
+ * @author David Spreekmeester | grrr.nl
+ * @modifiedby $LastChangedBy: $
+ * @version $Revision: $
+ * @package Garp
+ * @subpackage Content
+ * @lastmodified $Date: $
+ */
+class Garp_Content_Upload_Storage_Type_LocalWebserver extends Garp_Content_Upload_Storage_Type_Abstract {
+
+	/**
+	 * @return Garp_Content_Upload_FileList
+	 */
+	public function fetchFileList() {
+		$fileList = new Garp_Content_Upload_FileList();
+		
+		$configuredPaths = $this->_getConfiguredPaths();
+
+		$baseDir = $this->_getBaseDir();
+
+
+		foreach ($configuredPaths as $relPath) {
+			$absPath = $baseDir . $relPath;
+			if (file_exists($absPath)) {
+				if (!($dirList = scandir($absPath))) {
+					$this->_throwDirAccessError($absPath);
+				}
+
+				foreach ($dirList as $baseName) {
+					$fileList->addEntry($relPath . '/' . $baseName, filemtime($absPath));
+				}
+			} else Garp_Cli::errorOut("Warning: {$absPath} does not exist.");			
+		}
+		
+		return $fileList;
+	}
+
+
+	protected function _getBaseDir() {
+		return realpath(APPLICATION_PATH . '/../public');
+	}
+}
