1922f3d76fbf48769758ae55e99ec58e4df4e7f2
Peter Schilleman <peter@grrr.nl>
2013-03-05 14:25:51 +0100

    Narrowcast (Ticket #27)

---
+++ public/js/models/NarrowcastImage.js
@@ -0,0 +1,31 @@
+/** EXTENDED MODEL **/
+Garp.dataTypes.NarrowcastImage.on('init', function(){
+	/*
+
+	this.iconCls = 'icon-other-bogus';
+	Ext.apply(this.getColumn('some_id'),{
+		hidden: true,
+		header: __('Related bla'),
+		renderer: Garp.renderers.remoteDisplayFieldRenderer
+	});
+	
+	Ext.apply(this.getField('description'), {
+		width: 200,
+		anchor: false, // Always set to false if width override is desired
+		ref: '../../../descriptionReference',
+		fieldLabel: __('{1} is required if {2} is set', __('some field'), __('this value')),
+		xtype: 'textfield'
+	});
+	
+	this.getField('description').fieldLabel = __('Content:');
+	this.removeField('year');
+	
+	this.addListener('loaddata', function(rec, formPanel){
+		if (rec.status === 'someSpecialValue') {
+			formPanel.descriptionReference.hide();
+		}
+	});
+
+
+	*/
+});
