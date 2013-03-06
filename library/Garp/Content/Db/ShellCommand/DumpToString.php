9366bd9764c80ee3a20db660492ca5f55634c7cb
David Spreekmeester <david@grrr.nl>
2013-03-06 15:57:57 +0100

    sync script zet nu om naar lowercase

--- library/Garp/Content/Db/ShellCommand/DumpToString.php
+++ library/Garp/Content/Db/ShellCommand/DumpToString.php
@@ -7,10 +7,10 @@ class Garp_Content_Db_ShellCommand_DumpToString implements Garp_Content_Db_Shell
 	/**
 	 * @todo
 	 */
-	// const COMMAND_DUMP = "mysqldump -u'%s' -p'%s' --add-drop-table --host='%s' --databases %s --skip-comments";
+	const COMMAND_DUMP = "mysqldump -u'%s' -p'%s' --host='%s' --databases %s --add-drop-table --skip-comments";
 	// const COMMAND_DUMP = "mysqldump -u'%s' -p'%s' --host='%s' --databases %s --compact --add-drop-table";
 	// const COMMAND_DUMP = "mysqldump -u'%s' -p'%s' --host='%s' --databases %s --skip-comments --skip-opt --complete-insert --add-drop-table";
-	const COMMAND_DUMP = "mysqldump -u'%s' -p'%s' --host='%s' --databases %s --add-drop-table";
+	// const COMMAND_DUMP = "mysqldump -u'%s' -p'%s' --host='%s' --databases %s --add-drop-table";
 
 
 	/**
