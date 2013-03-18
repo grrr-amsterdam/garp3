5160a85cc678fbce21bfdc2dad6bc26d7b887940
Harmen Janssen <whatstyle@gmail.com>
2013-03-18 22:43:49 +0100

    Removed minification of assets

--- application/configs/garp_deploy.rb
+++ application/configs/garp_deploy.rb
@@ -42,7 +42,6 @@ namespace :deploy do
   desc "Deploy project"
   task :update do
     transaction do
-      minify_js
       update_code
       create_system_cache_dirs
       create_static_cache_dir
@@ -131,11 +130,6 @@ namespace :deploy do
 
 
   # ------- P R I V A T E   D E P L O Y   M E T H O D S
-  
-  desc "Minify local JS"
-  task :minify_js do
-      system('php garp/scripts/garp.php assets minifyJs')
-  end
   
   desc "Create backend cache directories"
   task :create_system_cache_dirs do
