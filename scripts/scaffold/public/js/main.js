729d611ed6016bad8f77b0adb0c362d80da32913
Harmen Janssen <whatstyle@gmail.com>
2013-04-04 00:28:07 +0200

    Better scaffold defaults

--- scripts/scaffold/public/js/main.js
+++ scripts/scaffold/public/js/main.js
@@ -1,5 +1,49 @@
+// Main app namespace
+var app = {
+	init: function() {
+		// Initialize all modules.
+	}
+};
+/**
+ * Responsive breakpoint registry
+ */
+app.responsive = {
+	BREAKPOINT_SMALL: 680,
+	BREAKPOINT_MEDIUM: 960,
+	BREAKPOINT_LARGE: 1200,
+	
+	/**
+	 * Read state of various breakpoints
+	 */
+	getCurrentBreakpoint: function() {
+		var tries = ['small', 'medium', 'large'];
+		var i = 0;
+		var bp = 'small';
+		
+		do {
+			bp = tries[i];
+		} while (app.responsive.matchesBreakpoint(tries[++i]));
+		return bp;
+	},
+	/**
+	 * Read state of various breakpoints
+	 */
+	matchesBreakpoint: function(breakpoint) {
+		switch (breakpoint) {
+			case 'small':
+				return document.documentElement.clientWidth >= this.BREAKPOINT_SMALL;
+			case 'medium':
+				return document.documentElement.clientWidth >= this.BREAKPOINT_MEDIUM;
+			case 'large':
+				return document.documentElement.clientWidth >= this.BREAKPOINT_LARGE;
+		}
+	}
+};
+
 $(function() {
-	var flashMsg = new Garp.FlashMessage({
+	app.init();
+
+	new Garp.FlashMessage({
 		parseCookie: true,
 		afterShow: function(fm){
 			fm.elm.bind('click', Garp.createDelegate(fm.close, fm));
