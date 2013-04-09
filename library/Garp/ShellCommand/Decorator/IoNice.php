f338979c358ad66a646ca8f7fa482a0f6fbb5f52
David Spreekmeester <david@grrr.nl>
2013-04-09 15:51:43 +0200

    refactoring: ShellCommand losgekoppeld

---
+++ library/Garp/ShellCommand/Decorator/IoNice.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Garp_ShellCommand_Decorator_IoNice
+ * @author David Spreekmeester | Grrr.nl
+ */
+class Garp_ShellCommand_Decorator_IoNice implements Garp_ShellCommand_Protocol {
+	const COMMAND_PREFIX_IONICE = 'ionice -c3 ';
+
+	/**
+	 * @var Garp_ShellCommand_Protocol $_command
+	 */
+	protected $_command;
+
+
+	public function __construct(Garp_ShellCommand_Protocol $command) {
+		$this->setCommand($command);
+	}
+	
+	/**
+	 * @return Garp_ShellCommand_Protocol
+	 */
+	public function getCommand() {
+		return $this->_command;
+	}
+	
+	/**
+	 * @param Garp_ShellCommand_Protocol $command
+	 */
+	public function setCommand($command) {
+		$this->_command = $command;
+	}
+
+
+	public function render() {
+		$command 		= $this->getCommand();
+		$commandString 	= $command->render();
+		$prefix			= self::COMMAND_PREFIX_IONICE;
+		
+		return $prefix . $commandString;
+	}
+	
+}
+
