74d1d13b5f61976a1c50a0ea6a2fbbb2bc0d8618
David Spreekmeester <david@grrr.nl>
2013-04-10 13:13:57 +0200

    garp update, shellcommands refactor

--- library/Garp/Cli/Command/Content.php
+++ library/Garp/Cli/Command/Content.php
@@ -8,9 +8,4 @@
  * @subpackage Cli
  * @lastmodified $Date: $
  */
-<<<<<<< HEAD
-exit('Please run this command through Golem.');
-=======
-
 throw new Exception('Please run this command through Golem.');
->>>>>>> 3e8461ef15175b1a6048e1152f07bd30426a9d2a
