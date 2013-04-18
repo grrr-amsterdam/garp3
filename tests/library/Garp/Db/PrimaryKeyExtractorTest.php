859d193320e98beb8cbc364196a9f59c26a32563
Harmen Janssen <whatstyle@gmail.com>
2013-04-18 16:38:58 +0200

    PK extractor tweaks + test

--- tests/library/Garp/Db/PrimaryKeyExtractorTest.php
+++ tests/library/Garp/Db/PrimaryKeyExtractorTest.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Tests Garp_Db_PrimaryKeyExtractorTest
+ * @group Db
+ */
 class Garp_Db_PrimaryKeyExtractorTest extends Garp_Test_PHPUnit_TestCase {
 	public function setUp() {
 		// Create bogus models
@@ -6,5 +10,21 @@ class Garp_Db_PrimaryKeyExtractorTest extends Garp_Test_PHPUnit_TestCase {
 			'name' => 'singular_pk',
 			'primary' => 'id'
 		));
+	}
+
+	public function testingSingularPkModel() {
+		$whereClauses = array(
+			'id = 1'      => array('id' => 1),
+			'id = 100'    => array('id' => 100),
+			'`id` = 1'    => array('id' => 1),
+			'id = "1"'    => array('id' => 1),
+			'id = \'1\''  => array('id' => 1),
+			'id = 1 AND name LIKE "%something%"' => array('id' => 1),
+			'`id` = 100 AND name = "harmen"' => array('id' => 100)
+		);
+		foreach ($whereClauses as $whereClause => $result) {
+			$pkExtractor = new Garp_Db_PrimaryKeyExtractor($this->_singularPkModel, $whereClause);
+			$this->assertEquals($pkExtractor->extract(), $result);
+		}
 	}
 }
