672aa0d57091ec4fabfc376194514c5cf4c62621
Harmen Janssen <whatstyle@gmail.com>
2013-04-22 12:01:17 +0200

    Translatable behavior

--- library/Garp/Model/Behavior/Translatable.php
+++ library/Garp/Model/Behavior/Translatable.php
@@ -28,7 +28,7 @@ class Garp_Model_Behavior_Translatable extends Garp_Model_Behavior_Abstract {
  	 * Suffix for i18n tables
  	 * @var String
  	 */
-	const I18N_TABLE_SUFFIX = '_i18n';
+	const I18N_MODEL_SUFFIX = 'I18n';
 
 	/**
  	 * The columns that can be translated
@@ -57,7 +57,7 @@ class Garp_Model_Behavior_Translatable extends Garp_Model_Behavior_Abstract {
  	 * @param Array $data The submitted data
  	 * @return Void
  	 */
-	protected function _beforeSave($data) {
+	protected function _beforeSave(&$data) {
 		foreach ($this->_translatableFields as $field) {
 			if (!empty($data[$field])) {
 				$this->_queue[$field] = $data[$field];
@@ -74,40 +74,47 @@ class Garp_Model_Behavior_Translatable extends Garp_Model_Behavior_Abstract {
 	protected function _afterSave(Garp_Model_Db $model, $primaryKeys) {
 		if ($this->_queue) {
 			$locales = Garp_I18n::getAllPossibleLocales();
-			array_walk($locales, array($this, '_saveI18nRecord'), $model, $primaryKeys);
+			foreach ($locales as $locale) {
+				$this->_saveI18nRecord($locale, $model, $primaryKeys);
+			}
 		}
 	}
 
 	/**
  	 * Save a new i18n record in the given language
  	 * @param String $language
- 	 * @param Garp_Model_Db $model
- 	 * @param Array $primaryKeys 
+	 * @param Garp_Model_Db $model 
+	 * @param Array $primaryKeys
  	 * @return Boolean
  	 */
 	protected function _saveI18nRecord($language, Garp_Model_Db $model, array $primaryKeys) {
-		$columns = array();
-		foreach ($this->_queue as $column => $data) {
-			if (!empty($data[$language])) {
-				$columns[$column] = $data[$language];
+		$data = array();
+		// Filter out the values in the right language
+		foreach ($this->_queue as $column => $value) {
+			if (!empty($value[$language])) {
+				$data[$column] = $value[$language];
 			}
 		}
-		$columns[self::LANG_COLUMN] = $language;
+		// Add the language
+		$data[self::LANG_COLUMN] = $language;
 
-		// @todo Figure out the foreign keys dynamically
-		$i18nModelName = $model->getName().self::I18N_TABLE_SUFFIX;
-		$primaryKeyColumns = $model->info(Zend_Db_Table_Abstract::PRIMARY);
-		foreach ($primaryKeys as $i => $primaryKey) {
-			if (empty($primaryKeyColumns[$i])) {
-				throw new Garp_Model_Behavior_Exception('Mismatched primary key values and columns. Column #'.$i.' not found in table.');
-			}
-			$foreignKey = $model->getName().'_'.$primaryKeyColumns[$i];
-			$columns[$foreignKey] = $primaryKey;
+		// Add the foreign key info to link the i18n data to the parent record
+		$i18nModel = $this->getI18nModel($model);
+		$referenceMap = $i18nModel->getReference(get_class($model));
+		$foreignKeyData = $this->_getForeignKeyData($referenceMap, $primaryKeys);
+		$data = array_merge($data, $foreignKeyData);
+
+		// Figure out wether to insert or update
+		$whereClause = $i18nModel->arrayToWhereClause(
+			array_merge($foreignKeyData, array(self::LANG_COLUMN => $language))
+		);
+
+		$row = $i18nModel->fetchRow($i18nModel->select()->where($whereClause));
+		if (!$row) {
+			$row = $i18nModel->createRow();
 		}
-
-		print "Inserting the following:\n";
-		print_r($columns);
-		exit;
+		$row->setFromArray($data);
+		$row->save();
 	}
 
 	/**
@@ -159,5 +166,33 @@ class Garp_Model_Behavior_Translatable extends Garp_Model_Behavior_Abstract {
 		$pkExtractor = new Garp_Db_PrimaryKeyExtractor($model, $where);
 		$pks = $pkExtractor->extract();
 		$this->_afterSave($model, $pks);
+	}
+
+	/**
+ 	 * Retrieve i18n model
+ 	 * @param Garp_Model_Db $model
+ 	 * @return Garp_Model_Db
+ 	 */
+	public function getI18nModel(Garp_Model_Db $model) {
+		$modelName = get_class($model);
+		$modelName .= self::I18N_MODEL_SUFFIX;
+		return new $modelName;
+	}
+
+	/**
+ 	 * Create array containing the foreign keys in the relationship mapped to the primary keys from the save.
+ 	 * @param Array $referenceMap The referenceMap describing the relationship
+ 	 * @param Arary $primaryKeys The given primary keys
+ 	 * @return Array 
+ 	 */
+	protected function _getForeignKeyData(array $referenceMap, array $primaryKeys) {
+		$data = array();
+		$foreignKeyColumns = $referenceMap['columns'];
+		if (count($foreignKeyColumns) !== count($primaryKeys)) {
+			throw new Garp_Model_Behavior_Exception('Not all foreign keys can be filled. I got '.
+				count($primaryKeys).' primary keys for '.count($foreignKeyColumns).' foreign keys.');
+		}
+		$data = array_combine($foreignKeyColumns, $primaryKeys);
+		return $data;
 	}
 }
