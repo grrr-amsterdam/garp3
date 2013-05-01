66442b837e12a0da20adb27db0692d249a2ec99d
Harmen Janssen <whatstyle@gmail.com>
2013-05-01 12:13:21 +0200

    Exclude snippets from cms where is_editable = false

--- application/modules/g/models/Snippet.php
+++ application/modules/g/models/Snippet.php
@@ -19,12 +19,33 @@ class G_Model_Snippet extends Model_Base_Snippet {
 		parent::init();
 	}
 	
-	
+	/**
+ 	 * Fetch a snippet by its identifier
+ 	 * @param String $identifier
+ 	 * @return Garp_Db_Table_Row
+ 	 */
 	public function fetchByIdentifier($identifier) {
-		if ($result = $this->fetchRow(
-			$this->select()->where('identifier = ?', $identifier)
-		)) {
+		$select = $this->select()->where('identifier = ?', $identifier);
+		if ($result = $this->fetchRow($select)) {
 			return $result;
-		} else throw new Exception('Snippet not found: '.$identifier);
+		}
+		throw new Exception('Snippet not found: '.$identifier);
+	}
+
+	/**
+ 	 * BeforeFetch: filters out snippets where is_editable = 0 in the CMs.
+ 	 * @param Array $args
+ 	 * @return Void
+ 	 */
+	public function beforeFetch(&$args) {
+		$model = &$args[0];
+		$select = &$args[1];
+		if (Zend_Registry::isRegistered('CMS') && Zend_Registry::get('CMS')) {
+			// Sanity check: this project might be spawned without the is_editable column,
+			// it was added to Snippet at May 1 2013.
+			if ($this->getFieldConfiguration('is_editable')) {
+				$select->where('is_editable = ?', 1);
+			}
+		}
 	}
 }
