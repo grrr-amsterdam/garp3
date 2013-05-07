189b3e26120bcc5ee2b668ee0e99197324f2df71
Harmen Janssen <whatstyle@gmail.com>
2013-05-07 13:51:07 +0200

    Tweaked Garp_Util_FullUrl so it can take named routes

--- library/Garp/Util/FullUrl.php
+++ library/Garp/Util/FullUrl.php
@@ -16,17 +16,16 @@ class Garp_Util_FullUrl {
  	 */
 	protected $_url;
 
-
 	/**
  	 * Class constructor
- 	 * @param String $baseUrl
+ 	 * @param String|Array $route String containing the path or Array containing route properties
+ 	 * 							  (@see Zend_View_Helper_Url for the format)
  	 * @param Boolean $omitProtocol Whether the protocol should be omitted, resulting in //www.example.com urls.
  	 * @return Void
  	 */
-	public function __construct($baseUrl, $omitProtocol = false) {
-		$this->_url = $this->_createFullUrl($baseUrl, $omitProtocol);
+	public function __construct($route, $omitProtocol = false) {
+		$this->_url = $this->_createFullUrl($route, $omitProtocol);
 	}
-
 
 	/**
  	 * Get the value
@@ -36,29 +35,58 @@ class Garp_Util_FullUrl {
 		return $this->_url;
 	}
 
-
 	/**
  	 * Create full URL
- 	 * @param String $baseUrl
+ 	 * @param String $route
  	 * @return String
  	 */
-	protected function _createFullUrl($baseUrl, $omitProtocol) {
+	protected function _createFullUrl($route, $omitProtocol) {
 		$application = Zend_Registry::get('application');
 		$bootstrap = $application->getBootstrap();
+		$viewObj = $bootstrap->getResource('view');
+
+		if (is_array($route)) {
+			$this->_validateRouteArray($route);
+			$router = Zend_Controller_Front::getInstance()->getRouter();
+			$route = $router->assemble($route[0], $route[1]);
+		} else {
+			$route = $viewObj->baseUrl($route);
+		}
 
 		if ($request = Zend_Controller_Front::getInstance()->getRequest()) {
 			$httpHost = $request->getHttpHost();
 			$url = $omitProtocol ? '' : $request->getScheme() . ':';
 		} else {
 			$ini = Garp_Cache_Ini::factory(APPLICATION_PATH.'/configs/application.ini');
-			if (isset($ini->cdn->domain) && $ini->cdn->domain) {
-				$httpHost = $ini->cdn->domain;
-			} else throw new Exception('ini.cdn.domain is not defined in application.ini.');
+			$this->_validateIniConfig($ini);
+			$httpHost = $ini->cdn->domain;
 			$url = $omitProtocol ? '' : 'http:';
 		}
 
-		$viewObj = $bootstrap->getResource('view');
-		$url .= '//' . $httpHost . $viewObj->baseUrl($baseUrl);
+		$url .= '//' . $httpHost . $route;
 		return $url;
+	}
+
+	/**
+ 	 * Check if given route array is valid.
+ 	 * @param Array $route
+ 	 * @return Boolean
+ 	 */
+	protected function _validateRouteArray(array $route) {
+		if (!array_key_exists(0, $route) || !array_key_exists(1, $route)) {
+			throw new Garp_Exception('Given route is invalid. Please provide an array '.
+				'with valid keys 0 and 1.');
+		}
+	}		
+
+	/**
+ 	 * Check ini config for required keys
+ 	 * @param Zend_Config $ini
+ 	 * @return Boolean
+ 	 */
+	protected function _validateIniConfig(Zend_Config $ini) {
+		if (!isset($ini->cdn->domain) || !$ini->cdn->domain) {
+			throw new Exception('ini.cdn.domain is not defined in application.ini.');
+		}
 	}
 }
