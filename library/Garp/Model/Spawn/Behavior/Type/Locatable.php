e88a922e013566511eb082c42ee5335214ae1585
David Spreekmeester <david@grrr.nl>
2013-05-08 02:11:49 +0200

    Refactored Behavior class.

---
+++ library/Garp/Model/Spawn/Behavior/Type/Locatable.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author David Spreekmeester | grrr.nl
+ */
+class Garp_Model_Spawn_Behavior_Type_Locatable extends Garp_Model_Spawn_Behavior_Type_Abstract {
+	protected $_fields = array(
+		'location_lat' => array(
+			'type' => 'numeric',
+			'float' => true,
+			'unsigned' => false,
+			'required' => false,
+			'editable' => true,
+			'visible' => false
+		),
+		'location_long' => array(
+			'type' => 'numeric',
+			'float' => true,
+			'unsigned' => false,
+			'required' => false,
+			'editable' => true,
+			'visible' => false
+		)
+	);
+}
