bde3902ca30643efc159a5484d9d6702b9ec69c5
David Spreekmeester <david@grrr.nl>
2013-05-08 02:56:31 +0200

    tab OCD for the win!

--- library/Garp/Model/Spawn/Behavior/Factory.php
+++ library/Garp/Model/Spawn/Behavior/Factory.php
@@ -7,16 +7,16 @@ class Garp_Model_Spawn_Behavior_Factory {
 
 
 	public function produce(Garp_Model_Spawn_Model_Abstract $model, $origin, $name, $params = null, $behaviorType = null) {
-		$behaviorClass = $this->_getBehaviorClass($name);
-		$behavior = new $behaviorClass($model, $origin, $name, $params, $behaviorType);
+		$behaviorClass 	= $this->_getBehaviorClass($name);
+		$behavior 		= new $behaviorClass($model, $origin, $name, $params, $behaviorType);
 		
 		return $behavior;
 	}
 
 	protected function _getBehaviorClass($name) {
-		$hasOwnClass = $this->_hasOwnClass($name);
-		$class = self::BEHAVIOR_NAMESPACE;
-		$class .= $hasOwnClass ? $name : 'Generic';
+		$hasOwnClass 	= $this->_hasOwnClass($name);
+		$class 			= self::BEHAVIOR_NAMESPACE;
+		$class 			.= $hasOwnClass ? $name : 'Generic';
 
 		return $class;
 	}
