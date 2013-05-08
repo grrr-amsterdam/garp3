6546627d826dbaaadac7f8b5c386557184465745
Peter Schilleman <peter@grrr.nl>
2013-05-08 18:45:49 +0200

    i18n in progress

--- public/js/models/Snippet.js
+++ public/js/models/Snippet.js
@@ -4,6 +4,110 @@ Garp.dataTypes.Snippet.on('init', function(){
 	this.disableDelete = true;
 	this.disableCreate = true;
 	
+	
+	// [TEMPORARILY]: SPAWNER HAS NO SUPPORT FOR i18n YET
+	
+	this.displayFieldRenderer = function(rec){
+		return rec.get('name') && rec.get('name').nl ? rec.get('name').nl : rec.get('identifier');
+	};
+ 
+	this.removeField('name');
+	this.removeField('html');
+	this.removeField('text');
+	this.addField({
+		name: 'name',
+		xtype: 'i18nsource'
+	});
+	this.addField({
+		name: 'html',
+		xtype: 'i18nsource'
+	});
+	this.addField({
+		name: 'text',
+		xtype: 'i18nsource'
+	});
+	
+	this.addField({
+		xtype: 'i18nfieldset',
+		title: __('Dutch'),
+		collapsed: false,
+		items: [{
+			name: '_name_nl',
+			fieldLabel: __('Name'),
+			disabled: false,
+			hidden: false,
+			maxLength: 124,
+			allowBlank: false,
+			xtype: 'textfield'
+		},{
+			name: '_html_nl',
+            fieldLabel: __('Html'),
+            disabled: false,
+            hidden: false,
+            enableMedia: false,
+            enableHeading: false,
+            enableSourceEdit: false,
+            enableEmbed: false,
+            enableAlignments: false,
+            enableColors: false,
+            enableFont: false,
+            enableFontSize: false,
+            enableUnderline: false,
+            enableBlockQuote: false,
+            enableDefinitionList: false,
+            height: 200,
+            allowBlank: true,
+            xtype: 'richtexteditor'
+		}, {
+			name: '_text_nl',
+            fieldLabel: __('Text'),
+            disabled: false,
+            hidden: false,
+            allowBlank: true,
+            xtype: 'textarea'
+		}]
+	});
+	this.addField({
+		xtype: 'i18nfieldset',
+		title: __('English'),
+		items: [{
+			name: '_name_en',
+			fieldLabel: __('Name'),
+			disabled: false,
+			hidden: false,
+			maxLength: 124,
+			xtype: 'textfield'
+		},{
+			name: '_html_en',
+            fieldLabel: __('Html'),
+            disabled: false,
+            hidden: false,
+            enableMedia: false,
+            enableHeading: false,
+            enableSourceEdit: false,
+            enableEmbed: false,
+            enableAlignments: false,
+            enableColors: false,
+            enableFont: false,
+            enableFontSize: false,
+            enableUnderline: false,
+            enableBlockQuote: false,
+            enableDefinitionList: false,
+            height: 200,
+            allowBlank: true,
+            xtype: 'richtexteditor'
+		}, {
+			name: '_text_en',
+            fieldLabel: __('Text'),
+            disabled: false,
+            hidden: false,
+            allowBlank: true,
+            xtype: 'textarea'
+		}]
+	});
+	// [/TEMPORARILY]
+	
+	
 	this.addListener('loaddata', function(rec, formPanel){
 		var form = formPanel.getForm();
 		var toggleFields = ['name', 'html', 'text'];
@@ -11,8 +115,9 @@ Garp.dataTypes.Snippet.on('init', function(){
 		// Show or hide fields based on their 'has_*' counterparts 
 		function toggleMode(){
 			Ext.each(toggleFields, function(fieldName){
-				var el = form.findField(fieldName).getEl().up('div.x-form-item');
-				el.setVisibilityMode(Ext.Element.DISPLAY).setVisible(rec.data['has_' + fieldName] == '1');
+				//var el = form.findField(fieldName).getEl().up('div.x-form-item');
+				//el.setVisibilityMode(Ext.Element.DISPLAY).setVisible(rec.data['has_' + fieldName] == '1');
+				form.findField(fieldName).setVisible(rec.data['has_' + fieldName] == '1');
 			});
 			// because imagePreview_image_id is not a formField but a button, we cannot find it with form.findField():
 			formPanel.ImagePreview_image_id.setVisible(rec.data.has_image == 1);
