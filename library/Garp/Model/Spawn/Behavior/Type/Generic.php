cf5996dd7ec002a21dfcf6f3300c68f3415b1f61
David Spreekmeester <david@grrr.nl>
2013-05-10 05:40:26 +0200

    Spawn verplaatst van Garp_Model_Spawn naar Garp_Spawn

--- library/Garp/Model/Spawn/Behavior/Type/Generic.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @author David Spreekmeester | grrr.nl
- */
-class Garp_Model_Spawn_Behavior_Type_Generic extends Garp_Model_Spawn_Behavior_Type_Abstract {
-
-}
--- library/Garp/Model/Spawn/MySql/Table/I18n.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @author David Spreekmeester | grrr.nl
- * @package Garp
- * @subpackage Spawn
- */
-class Garp_Model_Spawn_MySql_Table_I18n extends Garp_Model_Spawn_MySql_Table_Abstract {
-
-}
