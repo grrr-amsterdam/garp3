01b5a159695a99f9229a51170dfe23cd93b53ece
Harmen Janssen <whatstyle@gmail.com>
2013-05-16 17:18:53 +0200

    Tons o tweaks for gathering the alternate language URL

--- library/Garp/Application/Resource/Router.php
+++ library/Garp/Application/Resource/Router.php
@@ -50,58 +50,9 @@ class Garp_Application_Resource_Router extends Zend_Application_Resource_Router 
 			$defaultLocale = $defaultLocale[0];
 
 			$locales = $this->_getPossibleLocales();
-			$requiredLocalesRegex = '^('.join('|', $locales).')$';
-
 			$routes = $options['routes'];
-			foreach ($routes as $key => $value) {
-				// First let's add the default locale to this routes defaults.
-				$defaults = isset($value['defaults'])
-					? $value['defaults']
-					: array();
-				
-				// Always default all routes to the Zend_Locale default
-				$value['defaults'] = array_merge(array('locale' => $defaultLocale ), $defaults);
-
-				$routes[$key] = $value;
-
-				// Get our route and make sure to remove the first forward slash
-				// since it's not needed.
-				$routeString = $value['route'];
-				$routeString = ltrim($routeString, '/\\');
-
-				// Modify our normal route to have the locale parameter.
-				if (!isset($value['type']) || $value['type'] === 'Zend_Controller_Router_Route') {
-					$value['route'] = ':locale/'.$routeString;
-					$value['reqs']['locale'] = $requiredLocalesRegex;
-					$routes['locale_'.$key] = $value;
-				} else if ($value['type'] === 'Zend_Controller_Router_Route_Regex') {
-					$value['route'] = '('.join('|', $locales).')\/'.$routeString;
-
-					// Since we added the local regex match, we need to bump the existing
-					// match numbers plus one.
-					$map = isset($value['map']) ? $value['map'] : array();
-					foreach ($map as $index => $word) {
-						unset($map[$index++]);
-						$map[$index] = $word;
-					}
-
-					// Add our locale map
-					$map[1] = 'locale';
-					ksort($map);
-
-					$value['map'] = $map;
-
-					$routes['locale_'.$key] = $value;
-				} elseif ($value['type'] === 'Zend_Controller_Router_Route_Static') {
-					foreach ($locales as $locale) {
-						$value['route'] = $locale.'/'.$routeString;
-						$value['defaults']['locale'] = $locale;
-						$routes['locale_'.$locale.'_'.$key] = $value;
-					}
-				}
-			}
-
-			$options['routes'] = $routes;
+			$localizedRoutes = Garp_I18n::getLocalizedRoutes($routes, $locales);
+			$options['routes'] = array_merge($routes, $localizedRoutes);
 			$this->setOptions($options);
 		}
 
--- library/Garp/I18n.php
+++ library/Garp/I18n.php
@@ -47,4 +47,65 @@ class Garp_I18n {
 	public static function getLocales() {
 		return Zend_Controller_Front::getInstance()->getParam('locales');
 	}
+
+	/**
+ 	 * Generate localized versions of routes
+ 	 * @param Array $routes The originals
+ 	 * @param Array $locales
+ 	 * @return Array
+ 	 */
+	public static function getLocalizedRoutes(array $routes, array $locales) {
+		$localizedRoutes = array();
+		$defaultLocale = self::getDefaultLocale();
+		$requiredLocalesRegex = '^('.join('|', $locales).')$';
+		
+		foreach ($routes as $key => $value) {
+			// First let's add the default locale to this routes defaults.
+			$defaults = isset($value['defaults'])
+				? $value['defaults']
+				: array();
+
+			// Always default all routes to the Zend_Locale default
+			$value['defaults'] = array_merge(array('locale' => $defaultLocale ), $defaults);
+
+			//$routes[$key] = $value;
+
+			// Get our route and make sure to remove the first forward slash
+			// since it's not needed.
+			$routeString = $value['route'];
+			$routeString = ltrim($routeString, '/\\');
+
+			// Modify our normal route to have the locale parameter.
+			if (!isset($value['type']) || $value['type'] === 'Zend_Controller_Router_Route') {
+				$value['route'] = ':locale/'.$routeString;
+				$value['reqs']['locale'] = $requiredLocalesRegex;
+				$localizedRoutes['locale_'.$key] = $value;
+			} else if ($value['type'] === 'Zend_Controller_Router_Route_Regex') {
+				$value['route'] = '('.join('|', $locales).')\/'.$routeString;
+
+				// Since we added the local regex match, we need to bump the existing
+				// match numbers plus one.
+				$map = isset($value['map']) ? $value['map'] : array();
+				foreach ($map as $index => $word) {
+					unset($map[$index++]);
+					$map[$index] = $word;
+				}
+
+				// Add our locale map
+				$map[1] = 'locale';
+				ksort($map);
+
+				$value['map'] = $map;
+
+				$localizedRoutes['locale_'.$key] = $value;
+			} elseif ($value['type'] === 'Zend_Controller_Router_Route_Static') {
+				foreach ($locales as $locale) {
+					$value['route'] = $locale.'/'.$routeString;
+					$value['defaults']['locale'] = $locale;
+					$routes['locale_'.$locale.'_'.$key] = $value;
+				}
+			}
+		}
+		return $localizedRoutes;
+	}
 }
