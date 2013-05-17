57becd9b0a67b833492754917aef53ddafeaf694
David Spreekmeester <david@grrr.nl>
2013-05-17 17:41:04 +0200

    new commands

--- library/Garp/Shell/Command/Chgrp.php
+++ library/Garp/Shell/Command/Chgrp.php
@@ -3,13 +3,13 @@
  * Garp_Shell_Command_Chgrp
  * @author David Spreekmeester | Grrr.nl
  */
- class Garp_Shell_Command_Chgrp extends Garp_Shell_Command_Abstract {
+class Garp_Shell_Command_Chgrp extends Garp_Shell_Command_Abstract {
  	const COMMAND = "chgrp -R %s %s";
 	
 	/**
 	 * @var String $_group
 	 */
-	protected $_group; 
+	protected $_group;
 
 	/**
 	 * @var String $_path
