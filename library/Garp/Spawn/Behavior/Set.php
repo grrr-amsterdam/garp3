11e9e27c77a780496bfecf4b58259b769fdfdf0d
David Spreekmeester <david@grrr.nl>
2013-05-22 18:04:20 +0200

    refactored behaviors

--- library/Garp/Spawn/Behavior/Set.php
+++ library/Garp/Spawn/Behavior/Set.php
@@ -14,7 +14,17 @@ class Garp_Spawn_Behavior_Set {
 	 */
 	protected $_model;
 
-	protected $_defaultConditionalBehaviorNames = array('HtmlFilterable', 'NotEmpty', 'Email');
+	protected $_defaultConditionalBehaviorNames = array(
+		'HtmlFilterable',
+		'NotEmpty',
+		'Email',
+		'Translatable'
+	);
+	
+	protected $_validatorBehaviors = array(
+		'Email',
+		'NotEmpty'
+	);
 
 
 	public function __construct(Garp_Spawn_Model_Abstract $model, array $config) {
@@ -49,20 +59,37 @@ class Garp_Spawn_Behavior_Set {
 		$this->_model = $model;
 	}
 
-	protected function _add($origin, $behaviorName, $behaviorConfig = null, $behaviorType = null) {
+	protected function _add($origin, $behaviorName, $behaviorConfig = null) {
+		$behaviorType = $this->_isValidatorBehavior($behaviorName) ? 'Validator' : null;
+		
 		if (!array_key_exists($behaviorName, $this->_behaviors)) {
 			$factory 	= new Garp_Spawn_Behavior_Factory();
 			$behavior 	= $factory->produce($this->_model, $origin, $behaviorName, $behaviorConfig, $behaviorType);
 			$this->_behaviors[$behaviorName] = $behavior;
+
+			////////////////////
+			// if (
+			// 	$behaviorName === 'Translatable' &&
+			// 	$this->getModel()->id === 'Genre'
+			// ) {
+			// 	Zend_Debug::dump($this->getModel()->fields->getFields('origin', 'behavior'));
+			// 	exit;
+			// }
+			////////////////////
+
 
 			//	generate fields which are necessary for this behavior in the accompanying Model
 			$generatedFields = $this->_behaviors[$behaviorName]->getFields();
 			foreach ($generatedFields as $fieldName => $fieldParams) {
 				$this->_model->fields->add('behavior', $fieldName, $fieldParams);
 			}			
+
 		} else throw new Exception("The {$behaviorName} behavior is already registered.");
 	}
 
+	protected function _isValidatorBehavior($behaviorName) {
+		return in_array($behaviorName, $this->_validatorBehaviors);
+	}
 
 	protected function _loadConfiguredBehaviors(array $config) {
 		foreach ($config as $behaviorName => $behaviorConfig) {
@@ -98,68 +125,69 @@ class Garp_Spawn_Behavior_Set {
 	}
 
 	protected function _loadDefaultConditionalBehaviors() {
-		$behaviorConfig = null;
-		$behaviorType = null;
+		$model 				= $this->getModel();
+		$behaviorConfig 	= null;
+		$behaviorType 		= null;
 
 		foreach ($this->_defaultConditionalBehaviorNames as $behaviorName) {
-			switch ($behaviorName) {
-				case 'HtmlFilterable':
-					$htmlFieldNames = $this->_model->fields->getFieldNames('type', 'html');
-					if ($htmlFieldNames) {
-						$behaviorConfig = $htmlFieldNames;
-						$this->_add('default', $behaviorName, $behaviorConfig, $behaviorType);
-					}
-				break;
-				case 'NotEmpty':
-					$requiredFieldNames = $this->_getRequiredFieldNames();
-					if ($requiredFieldNames) {
-						$behaviorType = 'Validator';
-						$behaviorConfig = $requiredFieldNames;
-						$indexOfIdColumn = array_search('id', $behaviorConfig);
-						unset($behaviorConfig[$indexOfIdColumn]);
-						$this->_add('default', $behaviorName, $behaviorConfig, $behaviorType);
-					}
-				break;
-				case 'Email':
-					$emailFields = $this->_model->fields->getFields('type', 'email');
-					if ($emailFields) {
-						$validatableEmailFields = array();
-
-						foreach ($emailFields as $emailField) {
-							$validatableEmailFields[] = $emailField->name;
-						}
-
-						if ($validatableEmailFields) {
-							$behaviorType = 'Validator';
-							$behaviorConfig = $validatableEmailFields;
-							$this->_add('default', $behaviorName, $behaviorConfig, $behaviorType);
-						}
-					}
-				break;
-				default:
-					$this->_add('default', $behaviorName, $behaviorConfig, $behaviorType);
+			if (!$this->_needsConditionalBehavior($behaviorName)) {
+				continue;
 			}
+
+			// $behaviorConfig = $this->_getConditionalBehaviorConfig($behaviorName);
+			$this->_add('default', $behaviorName, $behaviorConfig, $behaviorType);
 		}
 	}
-	
+		
+	protected function _needsConditionalBehavior($behaviorName) {
+		$model = $this->getModel();
+		
+		switch ($behaviorName) {
+			case 'HtmlFilterable':
+				$htmlFieldNames = $model->fields->getFieldNames('type', 'html');
+				return (bool)$htmlFieldNames;
+			case 'NotEmpty':
+				$requiredFieldNames = $this->_getRequiredFieldNames();
+				return (bool)$requiredFieldNames;
+			case 'Email':
+				$emailFields = $model->fields->getFields('type', 'email');
+				if (!$emailFields) {
+					return false;
+				}
+
+				$emailFieldNames = array();
+				foreach ($emailFields as $emailField) {
+					$emailFieldNames[] = $emailField->name;
+				}
+
+				return (bool)$emailFieldNames;
+			case 'Translatable':
+				return $model->isMultilingual();
+		}
+	}
 
 	/**
-	 * Adds the weighable behavior, for user defined sorting of related objects. Can only be initialized after the relations for this model are set.
+	 * Adds the weighable behavior, for user defined sorting of related objects.
+	 * Can only be initialized after the relations for this model are set.
 	 */
 	protected function _addWeighableBehavior() {
-		$weighableRels = $this->_model->relations->getRelations('weighable', true);
+		$model = $this->getModel();
+		$weighableRels = $model->relations->getRelations('weighable', true);
 
-		if ($weighableRels) {
-			$weighableConfig = array();
-
-			foreach ($weighableRels as $relName => $rel) {
-				$weighableConfig[$relName] = array(
-					'foreignKeyColumn' => $rel->column,
-					'weightColumn' => Garp_Spawn_Util::camelcased2underscored($relName).'_weight'
-				);
-			}
-
-			$this->_add('relation', 'Weighable', $weighableConfig);
+		if (!$weighableRels) {
+			return;
 		}
+
+		$weighableConfig = array();
+
+		foreach ($weighableRels as $relName => $rel) {
+			$weightColumn = Garp_Spawn_Util::camelcased2underscored($relName) . '_weight';
+			$weighableConfig[$relName] = array(
+				'foreignKeyColumn' 	=> $rel->column,
+				'weightColumn' 		=> $weightColumn
+			);
+		}
+
+		$this->_add('relation', 'Weighable', $weighableConfig);
 	}
 }
--- library/Garp/Spawn/Behavior/Type/NotEmpty.php
+++ library/Garp/Spawn/Behavior/Type/NotEmpty.php
@@ -8,8 +8,8 @@ class Garp_Spawn_Behavior_Type_NotEmpty extends Garp_Spawn_Behavior_Type_Abstrac
 	 * In translated models (i18n leaves), multilingual columns should not be mandatory on PHP validator level.
 	 */
 	public function getParams() {
-		$model 			= $this->getModel();
-		$params 		= parent::getParams();
+		$model 	= $this->getModel();		
+		$params = $this->_getFieldNames();
 		
 		if (!$model->isTranslated()) {
 			return $params;
@@ -23,4 +23,41 @@ class Garp_Spawn_Behavior_Type_NotEmpty extends Garp_Spawn_Behavior_Type_Abstrac
 		$modelField = $this->getModel()->fields->getField($fieldName);
 		return !$modelField->isMultilingual();
 	}
+	
+	protected function _getFieldNames() {
+		$requiredFieldNames = $this->_getRequiredFieldNames();
+		if (!$requiredFieldNames) {
+			return;
+		}
+
+		$fieldNames = $requiredFieldNames;
+		$indexOfIdColumn = array_search('id', $fieldNames);
+		unset($fieldNames[$indexOfIdColumn]);
+		
+		return $fieldNames;
+	}
+	
+	protected function _getRequiredFieldNames() {
+		$model = $this->getModel();
+
+		if (!$model->isMultilingual()) {
+			return $this->_model->fields->getFieldNames('required', true);
+		}
+
+		return $this->_getUnilingualFieldNames();
+	}
+	
+	protected function _getUnilingualFieldNames() {
+		$unilingualFieldNames 	= array();
+		$requiredFields 		= $this->_model->fields->getFields('required', true);
+		
+		foreach ($requiredFields as $field) {
+			if (!$field->isMultilingual()) {
+				$unilingualFieldNames[] = $field->name;
+			}
+		}
+		
+		return $unilingualFieldNames;
+	}
+	
 }
