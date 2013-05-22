63fdbf60b639713be268845f0846fe14ed8adc91
David Spreekmeester <david@grrr.nl>
2013-05-22 18:37:56 +0200

    refactored behavior spawn

--- library/Garp/Spawn/Behavior/Type/Email.php
+++ library/Garp/Spawn/Behavior/Type/Email.php
@@ -3,6 +3,20 @@
  * @author David Spreekmeester | grrr.nl
  */
 class Garp_Spawn_Behavior_Type_Email extends Garp_Spawn_Behavior_Type_Abstract {
+
+	static public function isNeededBy(Garp_Spawn_Model_Abstract $model) {
+		$emailFields = $model->fields->getFields('type', 'email');
+		if (!$emailFields) {
+			return false;
+		}
+
+		$emailFieldNames = array();
+		foreach ($emailFields as $emailField) {
+			$emailFieldNames[] = $emailField->name;
+		}
+
+		return (bool)$emailFieldNames;
+	}
 	
 	/**
 	 * In translated models (i18n leaves), multilingual columns should not be mandatory on PHP validator level.
