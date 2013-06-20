ec12bce074ab72fe83f61d01bfda9f5e70277c1b
Harmen Janssen <whatstyle@gmail.com>
2013-06-20 15:53:21 +0200

    Deprecated Garp.Twitter

--- public/js/garp.front.js
+++ public/js/garp.front.js
@@ -1208,39 +1208,43 @@ Garp.relativeDate = function(oldest, newest, resolution) {
  *	twitter.search('garp');
  */
 Garp.Twitter = function(config){
+	if (typeof console !== 'undefined') {
+		console.warn('Garp.Twitter is deprecated because Twitter API version 1 is deprecated.');
+	}
+	return false;
 
-	// Default config: //
-	Garp.apply(this, {
-		query: '',
-		resultsPerPage: 25, // max 100
-		resultsAsArray: false,
-		searchTpl: '<img src="${1}" alt="${2}">${2}: ${3}<hr>',
-		listTpl: '<img src="${1}" alt="${2}">${2}: ${3}<hr>',
-		beforeFetch: jQuery.noop,
-		afterFetch: jQuery.noop,
-		onError: jQuery.noop // Gets called when no results found or an error occurred
-	});
+	//// Default config: //
+	//Garp.apply(this, {
+		//query: '',
+		//resultsPerPage: 25, // max 100
+		//resultsAsArray: false,
+		//searchTpl: '<img src="${1}" alt="${2}">${2}: ${3}<hr>',
+		//listTpl: '<img src="${1}" alt="${2}">${2}: ${3}<hr>',
+		//beforeFetch: jQuery.noop,
+		//afterFetch: jQuery.noop,
+		//onError: jQuery.noop // Gets called when no results found or an error occurred
+	//});
 	
-	// Override config: //
-	Garp.apply(this, config);
+	//// Override config: //
+	//Garp.apply(this, config);
 	
 	/**
 	* Searches Twitter for query and caches the query string for later re-use
 	* @param {query} (optional) query
 	*/
-	this.search = function(query){
-		if (query) {
-			this.query = query;
-		} else {
-			query = this.query;
-		}
-		query = encodeURIComponent(query);
-		var scope = this;
-		scope.beforeFetch.call(this);
-		$.getJSON(Garp.format('http://search.twitter.com/search.json?q=${1}&rpp=${2}&callback=?', query, this.resultsPerPage), function(response){
-			scope.parseResponse.call(scope, response);
-		});
-	};
+	//this.search = function(query){
+		//if (query) {
+			//this.query = query;
+		//} else {
+			//query = this.query;
+		//}
+		//query = encodeURIComponent(query);
+		//var scope = this;
+		//scope.beforeFetch.call(this);
+		//$.getJSON(Garp.format('http://search.twitter.com/search.json?q=${1}&rpp=${2}&callback=?', query, this.resultsPerPage), function(response){
+			//scope.parseResponse.call(scope, response);
+		//});
+	//};
 	
 	
 	/**
@@ -1248,47 +1252,47 @@ Garp.Twitter = function(config){
 	* @param {String} user
 	* @param {String} listId
 	*/
-	this.getList = function(user, listId){
-		var scope = this;
-		scope.beforeFetch.call(this);
-		$.getJSON(Garp.format('http://api.twitter.com/1/${1}/lists/${2}/statuses.json?callback=?', user, listId), function(response){
-			scope.parseResponse.call(scope, response);
-		});
-	};
+	//this.getList = function(user, listId){
+		//var scope = this;
+		//scope.beforeFetch.call(this);
+		//$.getJSON(Garp.format('http://api.twitter.com/1/${1}/lists/${2}/statuses.json?callback=?', user, listId), function(response){
+			//scope.parseResponse.call(scope, response);
+		//});
+	//};
 	
-	// private //
-	this.parseResponse = function(response){
-		var item, result = [];
-		if (response) {
-			if (response.results) { // search results
-				for (item in response.results) {
-					if (response.results[item].text) {
-						result.push(Garp.format(this.searchTpl, response.results[item].profile_image_url, response.results[item].from_user, response.results[item].text.replace(new RegExp('(http://[^ ]+)', "g"), '<a target="_blank" href="$1">$1</a>'), Garp.relativeDate(response.results[item].created_at, new Date()), response.results[item].from_user));
-					}
-				}
-			} else { // list results
-				var c = 1;
-				for (item in response) {
-					c++;
-					if (response[item].text) {
-						result.push(Garp.format(this.listTpl, response[item].user.profile_image_url, response[item].user.name, response[item].text.replace(new RegExp('(http://[^ ]+)', "g"), '<a href="$1">$1</a>'), Garp.relativeDate(response[item].created_at, new Date()), response[item].user.screen_name));
-					}
-					if(c > this.resultsPerPage){
-						break;
-					}
-				}
-			}
-			if (result.length) {
-				this.afterFetch(this.resultsAsArray ? result : result.join(''));
-			} else {
-				this.onError(response);
-			}
-		} else {
-			this.onError(response);
-		}
-	};
+	//// private //
+	//this.parseResponse = function(response){
+		//var item, result = [];
+		//if (response) {
+			//if (response.results) { // search results
+				//for (item in response.results) {
+					//if (response.results[item].text) {
+						//result.push(Garp.format(this.searchTpl, response.results[item].profile_image_url, response.results[item].from_user, response.results[item].text.replace(new RegExp('(http://[^ ]+)', "g"), '<a target="_blank" href="$1">$1</a>'), Garp.relativeDate(response.results[item].created_at, new Date()), response.results[item].from_user));
+					//}
+				//}
+			//} else { // list results
+				//var c = 1;
+				//for (item in response) {
+					//c++;
+					//if (response[item].text) {
+						//result.push(Garp.format(this.listTpl, response[item].user.profile_image_url, response[item].user.name, response[item].text.replace(new RegExp('(http://[^ ]+)', "g"), '<a href="$1">$1</a>'), Garp.relativeDate(response[item].created_at, new Date()), response[item].user.screen_name));
+					//}
+					//if(c > this.resultsPerPage){
+						//break;
+					//}
+				//}
+			//}
+			//if (result.length) {
+				//this.afterFetch(this.resultsAsArray ? result : result.join(''));
+			//} else {
+				//this.onError(response);
+			//}
+		//} else {
+			//this.onError(response);
+		//}
+	//};
 	
-	return this;
+	//return this;
 };
 
 /**
