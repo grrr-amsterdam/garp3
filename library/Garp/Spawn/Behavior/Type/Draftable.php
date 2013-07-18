369f3a4b80b9f3b81d1be94cea11cd81f49b7879
Harmen Janssen <whatstyle@gmail.com>
2013-07-18 17:29:41 +0200

    Refactored Draftable slightly to allow for non-published use

--- library/Garp/Spawn/Behavior/Type/Draftable.php
+++ library/Garp/Spawn/Behavior/Type/Draftable.php
@@ -16,4 +16,29 @@ class Garp_Spawn_Behavior_Type_Draftable extends Garp_Spawn_Behavior_Type_Abstra
 			'required' => false
 		)
 	);
+
+	public function __construct(Garp_Spawn_Model_Abstract $model, $origin, $name, $params = null, $type = null) {
+		if (!empty($params['default'])) {
+			$this->_setStatusDefault($params['default']);
+		}
+
+		// Use only the online_status part of Draftable, discard published
+		if (!empty($params['draft_only'])) {
+			unset($this->_fields['published']);
+		}
+
+		parent::__construct($model, $origin, $name, $params, $type);
+	}
+
+	/**
+ 	 * If default == 'draft', online_status default is 0.
+ 	 * If it's 'online', it is 1.
+ 	 * @param String $status
+ 	 * @return Void
+ 	 */
+	public function _setStatusDefault($default) {
+		if ('draft' === $default) {
+			$this->_fields['online_status']['default'] = 0;
+		}
+	}
 }
