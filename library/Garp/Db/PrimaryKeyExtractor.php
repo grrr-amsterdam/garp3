3e2b708cd662d41f32cc1814e7d4d94fb0603708
Harmen Janssen <whatstyle@gmail.com>
2013-07-22 15:49:41 +0200

    Tweaked primary key extractor to allow for more WHERE clause syntaxes

--- library/Garp/Db/PrimaryKeyExtractor.php
+++ library/Garp/Db/PrimaryKeyExtractor.php
@@ -26,6 +26,9 @@ class Garp_Db_PrimaryKeyExtractor {
  	 */
 	public function __construct(Zend_Db_Table_Abstract $model, $where) {
 		$this->_model = $model;
+		if (is_array($where)) {
+			$where = implode(' AND ', $where);
+		}
 		$this->_where = $where;
 	}
 
@@ -34,24 +37,25 @@ class Garp_Db_PrimaryKeyExtractor {
 	 * @return Array
 	 */
 	public function extract() {
-		if (is_array($this->_where)) {
-			$this->_where = implode(' AND ', $this->_where);
-		}
 		$pkColumns = $this->_model->info(Zend_Db_Table_Abstract::PRIMARY);
 		$pkValues = array();
 		$table = $this->_model->getName();
+		$where = $this->_where;
+		// Lose the parentheses, they mean nothing to us (more importantly, "((key = value))" fails)
+		// @fixme A string primary key containing a "(" will not be returned. 
+		$where = str_replace(array('(', ')'), '', $where);
 		foreach ($pkColumns as $pk) {
 			$regexp = '/(?:`?'.preg_quote($table).'`?\.|\s|^){1}`?(?:'.preg_quote($pk).')`?\s?=\s?(?:(?P<q>[\'"])(?P<value>(?:(?!\k<q>).)*)\k<q>|(?P<rest>\w*))/';
-			if (preg_match($regexp, $this->_where, $matches)) {
-				// Note: backreference "rest" is there to catch unquoted
-				// values. (id = 100 instead of id = "100")
-				if (!empty($matches['rest'])) {
-					$value = $matches['rest'];
-				} else {
-					$value = $matches['value'];
-				}
-				$pkValues[$pk] = $value;
+			if (!preg_match($regexp, $where, $matches)) {
+				continue;
 			}
+			// Note: backreference "rest" is there to catch unquoted
+			// values. (id = 100 instead of id = "100")
+			$value = $matches['value'];
+			if (!empty($matches['rest'])) {
+				$value = $matches['rest'];
+			}
+			$pkValues[$pk] = $value;
 		}
 		return $pkValues;
 	}
