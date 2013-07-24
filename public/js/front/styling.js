fdbeb28708600e6641921b8bc4e8b65342f5eeda
Harmen Janssen <whatstyle@gmail.com>
2013-07-24 23:51:43 +0200

    Separate Garp.front modules

--- public/js/front/styling.js
+++ public/js/front/styling.js
@@ -5,6 +5,21 @@ if (typeof Garp === 'undefined') {
 	var Garp = {};
 }
 
+Garp.transitionEndEvents = [
+	'transitionEnd',
+	'oTransitionEnd',
+	'msTransitionEnd',
+	'transitionend',
+	'webkitTransitionEnd'
+];
+
+Garp.animationEndEvents = [
+	'animationend',
+	'webkitAnimationEnd',
+	'oanimationend',
+	'MSAnimationEnd'
+];
+
 Garp.getStyle = function(elm, rule) {
 	if (document.defaultView && document.defaultView.getComputedStyle) {
 		return document.defaultView.getComputedStyle(elm, '').getPropertyValue(rule);
@@ -42,17 +57,42 @@ Garp.getTransitionProperty = function() {
 	return null;
 };
 
+Garp.getAnimationProperty = function() {
+	var el = document.createElement('fakeelement');
+	var animations = [
+		'animationName',
+		'OAnimationName',
+		'MSAnimationName',
+		'MozAnimationName',
+		'WebkitAnimationName'
+	];
+	var getCurriedFunction = function(a) {
+		return function() {
+			return a;
+		};
+	};
+	for (var i = 0, la = animations.length; i < la; ++i) {
+		if (el.style[animations[i]] !== undefined) {
+			// Speed up subsequent calls
+			Garp.getAnimationProperty = getCurriedFunction(animations[i]);
+			return animations[i];
+		}
+	}
+	return null;
+};
+
 /**
  * Get cross-browser transitionEnd event
  * Inspiration: @see http://stackoverflow.com/questions/5023514/how-do-i-normalize-css3-transition-functions-across-browsers
+ * Note: this is not entirely reliable, Chrome uses 'transition', but listens to the WebkitTransitionEnd event. Some versions that is...
  */
 Garp.getTransitionEndEvent = function() {
 	var transitions = {
-		'transition':'transitionEnd',
-		'OTransition':'oTransitionEnd',
-		'MSTransition':'msTransitionEnd',
-		'MozTransition':'transitionend',
-		'WebkitTransition':'webkitTransitionEnd'
+		'transition': 'transitionEnd',
+		'OTransition': 'oTransitionEnd',
+		'MSTransition': 'msTransitionEnd',
+		'MozTransition': 'transitionend',
+		'WebkitTransition': 'webkitTransitionEnd'
 	};
 	var t = Garp.getTransitionProperty();
 	var getCurriedFunction = function(t) {
