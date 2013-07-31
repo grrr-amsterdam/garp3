c7e4bede15b9552d37b8c42bdf06a1ee32b796e0
David Spreekmeester <david@grrr.nl>
2013-07-31 18:14:50 +0200

    expanding tests

---
+++ tests/library/Garp/Spawn/MySql/ManagerTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author David Spreekmeester | Grrr.nl
+ * This class tests Garp_Spawn_MySql_Manager.
+ * @group Spawn
+ */
+class Garp_Spawn_MySql_ManagerTest extends PHPUnit_Framework_TestCase {
+	// protected $_mocks = array(
+	// 	'directory' => null,
+	// 	'modelName' => 'Bogus',
+	// 	'extension' => 'json',
+	// 	'sql'		=> null
+	// );
+
+
+	// public function setUp() {
+	// 	$this->_mocks['directory'] = APPLICATION_PATH . "/../garp/application/modules/mocks/models/config/";
+	// 	$this->_mocks['sql'] = file_get_contents($this->_mocks['directory'] . '../sql/bogus.sql');
+	// }
+
+	// function testHabtmTablesExist() {
+
+	// 	$dbAdapter = Zend_Db_Table::getDefaultAdapter();
+	// 	$dbAdapter->query('select * from bogus');
+	// }
+
+}
