24cce69693b7cc6a675b8ce60048aad593367633
David Spreekmeester <david@grrr.nl>
2013-10-02 15:09:41 +0200

    fixing Elastic unit tests

--- library/Garp/Service/Elasticsearch/Response.php
+++ library/Garp/Service/Elasticsearch/Response.php
@@ -9,7 +9,10 @@
  * @lastmodified $Date: $
  */
 class Garp_Service_Elasticsearch_Response {
-	const ERROR_UNKNOWN_ERROR = 'Could not retrieve error message.';
+	const ERROR_NOT_FOUND =
+		"I can't find what you were looking for.";
+	const ERROR_UNKNOWN_ERROR = 
+		'Could not retrieve error message.';
 
 	/**
 	 * @var Zend_Http_Response $_httpResponse
@@ -23,6 +26,7 @@ class Garp_Service_Elasticsearch_Response {
 		$this->setHttpResponse($httpResponse);
 
 		if (!$this->isOk()) {
+			// Zend_Debug::dump($httpResponse); exit;
 			throw new Exception($this->getError());
 		}
 	}
@@ -41,13 +45,18 @@ class Garp_Service_Elasticsearch_Response {
 	 * @return String The error message. Throws Exception if message cannot be retrieved.
 	 */
 	public function getError() {
-		$bodyJson = $this->getBody();
-		$body = json_decode($bodyJson, true);
-		if (!array_key_exists('error', $body)) {
-			throw new Exception(self::ERROR_UNKNOWN_ERROR);
+		$bodyJson 	= $this->getBody();
+		$body 		= json_decode($bodyJson, true);
+		
+		if (array_key_exists('error', $body)) {
+			return $body['error'];
 		}
 
-		return $body['error'];
+		if ($this->getHttpResponse()->getStatus() === 404) {
+			throw new Exception(self::ERROR_NOT_FOUND . ' ' . $this->getBody());
+		}
+
+		throw new Exception(self::ERROR_UNKNOWN_ERROR);
 	}
 
 	/**
