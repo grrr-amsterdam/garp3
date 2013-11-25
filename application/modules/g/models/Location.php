1dcdb68b8eadf0e0385037c20a9c0a85359bb151
David Spreekmeester <david@grrr.nl>
2013-11-25 23:00:15 +0100

    Location based on 6PP, combined with Google Maps API calls and local database caching

---
+++ application/modules/g/models/Location.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * G_Model_Location
+ * Standard implementation of a (Dutch) location, based on the 6PP postal code database.
+ * @author David Spreekmeester | grrr.nl
+ * @modifiedby $LastChangedBy: $
+ * @version $Revision: $
+ * @package Garp
+ * @lastmodified $Date: $
+ */
+class G_Model_Location extends Model_Base_Location {
+
+	/**
+ 	 * Fetches the location record from the database if it exists.
+ 	 * If not, a call is made to the Google Maps API,
+ 	 * and the result is stored in the database.
+ 	 */
+	public function fetchRowByZip($zip) {
+		$zip = $this->_normalizeZip($zip);
+		$row = $this->_fetchRowByZipFromDatabase($zip);
+
+		if ($row) {
+			return $row;
+		}
+
+		$googleLocation = $this->_fetchRowByZipFromGoogle($zip);
+
+		if (!$googleLocation) {
+			throw new Exception("Could not find location.");
+		} 
+
+//exit('<hr>Deze wel...');
+		$this->_storeLocation($zip, $googleLocation);
+//exit('<hr>Deze niet...');
+		$row = $this->_fetchRowByZipFromDatabase($zip);
+
+		return $row;
+	}
+
+	/**
+ 	 * Normalize the input so that it matches the stored format.
+ 	 */
+	protected function _normalizeZip($zip) {
+		if (strlen($zip) === 6) {
+			$zip = substr($zip, 0, 4) . ' ' . substr($zip, 4, 2);
+		}
+
+		return $zip;
+	}
+
+	protected function _fetchRowByZipFromDatabase($zip) {
+		$select = $this->select()->where('zip = ?', $zip);
+		$row = $this->fetchRow($select);
+
+		return $row;
+	}
+
+	/**
+ 	 * @param String $zip
+ 	 * @return Garp_Service_Google_Maps_Response
+ 	 */
+	protected function _fetchRowByZipFromGoogle($zip) {
+		$api = new Garp_Service_Google_Maps();
+		$response = $api->fetchLocation($zip);
+
+		return $response;
+	}
+
+	protected function _storeLocation($zip, Garp_Service_Google_Maps_Response $location) {
+		$row = (array)$location;
+		$row['zip'] = $zip;
+//exit('wut');
+		$this->insert($row);
+	}
+}
