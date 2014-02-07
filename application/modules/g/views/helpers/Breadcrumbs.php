8e2731328c8f230191858c132813af93597ce1f5
Mattijs <mattijs@grrr.nl>
2014-02-07 11:41:54 +0100

    -n added wrapper div to breamcrumbs helper

--- application/modules/g/views/helpers/Breadcrumbs.php
+++ application/modules/g/views/helpers/Breadcrumbs.php
@@ -8,12 +8,13 @@
  * @package      G_View_Helper
  */
 class G_View_Helper_Breadcrumbs extends Zend_View_Helper_Abstract {
-	public function breadcrumbs(array $links) {
-		$out = '<ol class="breadcrumbs mb-half">'."\n";
+	public function breadcrumbs(array $links, $includeWrapper = false) {
+
+		$out = '<div class="breadcrumbs">'."\n".'<ol class="breadcrumbs__list mb-half">'."\n";
 		foreach ($links as $url => $label) {
 			$out .= "\t<li>{$this->view->htmlLink($url, $label)}</li>\n";
 		}
-		$out .= "</ol>\n";
+		$out .= "</ol>\n</div>";
 		return $out;
 	}
 }
