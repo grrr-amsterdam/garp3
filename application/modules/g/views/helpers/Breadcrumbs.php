13a351830ec77ecf78084fe0a3df38e30e2dbb15
Mattijs <mattijs@grrr.nl>
2014-02-07 11:11:58 +0100

    -n added breacrumbs helper

---
+++ application/modules/g/views/helpers/Breadcrumbs.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Garp_View_Helper_Breadcrumbs
+ * Prints breadcrumbs.
+ *
+ * @author       Harmen Janssen | grrr.nl
+ * @version      1.0
+ * @package      Garp_View_Helper
+ */
+class Garp_View_Helper_Breadcrumbs extends Zend_View_Helper_Abstract {
+	public function breadcrumbs(array $links) {
+		$out = '<ol class="breadcrumbs mb-half">'."\n";
+		foreach ($links as $url => $label) {
+			$out .= "\t<li>{$this->view->htmlLink($url, $label)}</li>\n";
+		}
+		$out .= "</ol>\n";
+		return $out;
+	}
+}
