1cad85d53e2ddfd1298d6b42160b5cd4cfb5236c
David Spreekmeester <david@grrr.nl>
2014-03-12 11:34:50 +0100

    fixed icons and translations that forked incorrectly from flattening Garp

--- public/js/locale/garp-nl.js
+++ public/js/locale/garp-nl.js
@@ -5,20 +5,20 @@
  */
 Ext.ns('Garp', 'Garp.locale');
 
-Ext.apply(Garp.locale, {
+Garp.locale = {
 	'Choose content type': 'Kies soort inhoud',
 	'Would you like to save your changes?': 'Wilt u uw wijzigingen opslaan?',
 	'File': 'Bestand',
 	'Edit': 'Bewerken',
 	'View': 'Beeld',
-	'Number of items per page': 'Aantal items per pagina',
+	'Number of items per page' : 'Aantal items per pagina',
 	'About Garp': 'Over Garp',
 	'New': 'Nieuw',
 	'Save': 'Opslaan',
 	'Delete': 'Verwijderen',
 	'Duplicate': 'Dupliceren',
 	'Are you sure you want to delete the selected item?': 'Weet u zeker dat u het geselecteerde item wilt verwijderen?',
-	'Are you sure you want to delete the selected items?': 'Weet u zeker dat u de geselecteerde items wilt verwijderen?',
+	'Are you sure you want to delete the selected items?':'Weet u zeker dat u de geselecteerde items wilt verwijderen?',
 	'Extra': 'Extra',
 	'Refresh': 'Vernieuwen',
 	'Export': 'Exporteren',
@@ -37,10 +37,6 @@ Ext.apply(Garp.locale, {
 	'1 item maximum': 'Maximaal 1 item',
 	'Relate or reorder items by dragging them around, or use the arrow buttons in the middle.': 'Relateer of sorteer items door ze te slepen, of maak gebruik van de knoppen met pijltjes in het midden.',
 	'Open in new window': 'Open in nieuw venster',
-	'Preview': 'Preview',
-	'Preview this item': 'Bekijk voorbeeld',
-	'To preview, save this item first': 'Sla dit item eerst op om een voorbeeld te kunnen zien',
-	
 	'Click here to view all items again': 'Klik hier om opnieuw alle items weer te geven',
 	'Relating&hellip;': 'Bezig met relateren&hellip;',
 	
@@ -51,15 +47,15 @@ Ext.apply(Garp.locale, {
 	'<b>Add Blockquote</b><br>Convert selected text into a blockquote.': '<b>Citaat invoegen</b><br>Maak van de geselecteerde tekst een citaat.',
 	'<b>Add Heading</b><br>Convert selected text into a heading.': '<b>Kopje invoegen</b><br>Maak van de geselecteerde tekst een kopje.',
 	'<b>Add Link</b><br>Convert selected text into a hyperlink.': '<b>Link invoegen</b><br>Maak van de geselecteerde tekst een hyperlink.',
-	'<b>Add embed</b>': '<b>Embed invoegen</b>',
+	'<b>Add embed</b>' : '<b>Embed invoegen</b>',
 	'Add link': 'Link toevoegen',
 	'Title': 'Titel',
-	'<b>Image</b><br>Insert image.': '<b>Afbeelding</b><br>Afbeelding invoegen.',
+	'<b>Image</b><br>Insert image.' : '<b>Afbeelding</b><br>Afbeelding invoegen.',
 	
-	'Step': 'Stap',
-	'of': 'van',
+	'Step' : 'Stap',
+	'of' : 'van',
 	'Specify an image or add new one': 'Geef een afbeelding op, of voeg een nieuwe toe',
-	'Specify a crop template, set aligning and/or add a caption': 'Kies een formaat, stel uitlijning in en/of voeg een bijschrift toe',
+	'Specify a crop template, set aligning and/or add a caption':'Geef een crop template aan, stel uitlijning in en/of voeg een bijschrift toe',
 	
 	'Crop': 'Crop',
 	'Alignment': 'Uitlijning',
@@ -71,42 +67,80 @@ Ext.apply(Garp.locale, {
 	'Paste as plain text': 'Plakken zonder opmaak',
 	'Not a valid Url': 'Geen geldige Url',
 	
+	'Identifier': 'Kenmerk',
+	
 	'Ok': 'Ok',
 	'Cancel': 'Annuleren',
 	'Ready': 'Gereed',
 	'Browse&hellip;': 'Bladeren&hellip;',
 	'Error uploading file': 'Fout tijdens uploaden van bestand',
 	
-	'Image Cropper': 'Afbeelding bijsnijden',
+	'Image Cropper' : 'Afbeelding bijsnijden',
 	'Crop template': 'Vaste verhouding',
-	'Crop manually': 'Eigen verhouding',
-	'Preview': 'Voorbeeld',
-	'Drag the selection, or drag the corners to resize the selection.': 'Sleep de selectie, of sleep de hoeken om de grootte aan te passen.',
+	'Crop manually' : 'Eigen verhouding',
+	'Preview' : 'Voorbeeld',
+	'Drag the selection, or drag the corners to resize the selection.' : 'Sleep de selectie, of sleep de hoeken om de grootte aan te passen.',
 	
 	'no': 'nee',
 	'yes': 'ja',
-	'(empty)': '(leeg)',
+	'(empty)' : '(leeg)',
 	
+	'Activity': 'Activiteit',
+	'New Activity': 'Nieuwe Activiteit',
 	'Is archived': 'Is gearchiveerd',
 	'Start date': 'Begindatum',
 	'Start time': 'Begintijd',
 	'End date': 'Einddatum',
 	'End time': 'Eindtijd',
-	'Invalid date': 'Ongeldige datum',
-	'Today at': 'Vandaag om',
-	'Yesterday at': 'Gisteren om',
-	'Published': 'Gepubliceerd',
-	'No date specified': 'Geen datum opgegeven',
 	
+	'Article': 'Artikel',
+	'New Article': 'Nieuw Artikel',
+	'Headline' : 'Kop',
+	'Excerpt': 'Samenvatting',
+	'Twitter description': 'Twitter beschrijving',
+	'Content type': 'Soort Content',
+	'Motivation': 'Motivatie',
+	'May appear on home': 'Mag op homepage',
+	'Biography': 'Biografie',
+	'Priority': 'Prioriteit',
+	
+	'New Best Practice': 'Nieuwe Best Practice',
+	
+	'Best Practice Category': 'Best Practice Categorie',
+	'New Best Practice Category': 'Nieuwe Best Practice Categorie',
+	
+	'Best Practice Client': 'Best Practice Afnemer',
+	'New Best Practice Client': 'Nieuwe Best Practice Afnemer',
+	
+	'Best Practice Creator': 'Best Practice Maker',
+	'New Best Practice Creator': 'Nieuwe Best Practice Maker',
+	
+	'New Best Practice Set': 'Nieuwe Best Practice Set',
+	
+	'External Url': 'Externe URL',
+	'External url': 'Externe URL',
+	'New External Url': 'Nieuwe Externe URL',
+	
+	'Fact Figure': 'Feiten & Cijfers',
+	'New Fact Figure': 'Nieuw Feit & Cijfers',
+	
+	'Organisational Person': 'Overige personen',
+	'New Organisational Person': 'Nieuw persoon',
+	'Subtitle nl': 'Subtitel nl',
+	'Subtitle en': 'Subtitel en',
+	
+	'Static Page': 'Statische Pagina',
+	'New Static Page': 'Nieuwe Statische Pagina',
+	
+	'User': 'Gebruiker',
+	'New User': 'Nieuwe gebruiker',
+	
+	'News': 'Nieuws',
+	'New News Item': 'Nieuw Nieuwsitem',
+	'Name': 'Naam',
 	'Description': 'Beschrijving',
 	'Created': 'Aangemaakt',
-	'Modified': 'Bewerkt',
-	
-	'Map': 'Kaart',
-	'Add Location': 'Lokatie aangeven',
-	'Remove Location': 'Lokatie verwijderen',
-	'Address Lookup': 'Adres opzoeken',
-	'Visibility': 'Zichtbaarheid',
+	'Modified': 'Aangepast',
 	
 	'Image': 'Afbeelding',
 	'New Image': 'Nieuwe afbeelding',
@@ -114,41 +148,6 @@ Ext.apply(Garp.locale, {
 	'Only zip files with a maximum of 20 MB are accepted': 'Alleen zip bestanden van maximaal 20 MB zijn toegestaan',
 	'Only pdf files with a maximum of 20 MB are accepted': 'Alleen pdf bestanden van maximaal 20 MB zijn toegestaan',
 	'caption': 'onderschrift',
-	
-	'Snippet': 'Snippet',
-	'New Snippet': 'Nieuw snippet',
-	'Small pieces of text, images or other media used on the website.': 'Tekstfragmenten, afbeeldingen en andere media op de website.',
-	'Name': 'Naam',
-	'Identifier': 'Kenmerk',
-	'Description': 'Beschrijving',
-	'Content': 'Content',
-	'Text': 'Tekst',
-	'Has name': 'Heeft naam',
-	'Has html': 'Heeft html',
-	'Has image': 'Heeft afbeelding',
-	'Has text': 'Heeft tekst',
-	'ID': 'Id',
-	'Uri': 'Uri',
-	'Created': 'Aangemaakt',
-	'Modified': 'Aangepast',
-	'Player': 'Player',
-	'Duration': 'Duur',
-	'Tags': 'Tags',
-	'Thumbnail': 'Thumbnail',
-	'Slug': 'Kenmerk',
-	'Author id': 'Auteur id',
-	'Modifier id': 'Bewerker id',
-	'Caption': 'Bijschrift',
-	'Full name': 'Volledige naam',
-	'First name': 'Voornaam',
-	'Last name prefix': 'Tussenvoegsel',
-	'Last name': 'Achternaam',
-	'Subtitle': 'Subtitel',
-	'Role': 'Rol',
-	'Author': 'Auteur',
-		
-	'Clear selection': 'Selectie opheffen',
-	'Search in:': 'Zoeken in:',
 	
 	'Sort index': 'Volgnummer',
 	'Visible in list': 'Zichtbaar in lijst',
@@ -158,7 +157,12 @@ Ext.apply(Garp.locale, {
 	'No Image uploaded': 'Geen afbeelding aanwezig',
 	'Please specify an image': 'Geef een afbeelding op',
 	'Select file': 'Geef bestand op',
-	'View original file': 'Toon originele afbeelding',
+	
+	'Map': 'Kaart',
+	'Add Location': 'Lokatie aangeven',
+	'Remove Location': 'Lokatie verwijderen',
+	'Address Lookup': 'Adres opzoeken',
+	'Visibility': 'Zichtbaarheid',
 	
 	'Specify selection to export (1/3)': 'Geef selectie op om te exporteren (1/3)',
 	'Currently selected item': 'Huidige item',
@@ -181,5 +185,214 @@ Ext.apply(Garp.locale, {
 	'Excel and CSV filetypes are supported': 'Excel en CSV bestanden worden ondersteund',
 	'Please wait': 'Moment geduld',
 	'Fields': 'Velden',
-	'Total': 'Totaal'
-});
+	
+	//'Total {count} item(s):' : '{count} item(s) in totaal',
+	'Total' : 'Totaal',
+	//// ////
+	
+	'New Snippet': 'Nieuw snippet',
+	'Small pieces of text, images or other media used on the website.' : 'Tekstfragmenten, afbeeldingen en andere media op de website.',
+	
+	'CollectionItem': 'Topstuk',
+	'New Collection Item': 'Nieuw topstuk',
+	'Year': 'Jaartal',
+	
+	'Event': 'Event',
+	'New Event': 'Nieuw event',
+	'Is educational': 'Is educatief',
+	
+	'Exposition': 'Tentoonstelling',
+	'New Exposition': 'Nieuwe tentoonstelling',
+	'Subtitle': 'Subtitel',
+	'Start': 'Start',
+	'End': 'Eind',
+	'Show on Home': 'Op homepage tonen',
+	
+	'Employee': 'Medewerker',
+	'New Employee': 'Nieuwe medewerker',
+	'Function':'Functie',
+	'Function nl': 'Functie nl',
+	'Function en': 'Functie en',
+	
+	'Person': 'Persoon',
+	'New Person': 'Nieuw persoon',
+	
+	'Deal': 'Arrangement',
+	'New deal': 'Nieuw arrangement',
+	
+	'Faq': 'Veel Gestelde Vragen',
+	'New Question': 'Nieuwe vraag',
+	
+	'Question': 'Vraag',
+	'Answer': 'Antwoord',
+	
+	'PressDocument': 'Persdocument',
+	'New Press Document': 'Nieuw persdocument',
+	
+	'JobOpening': 'Vacature',
+	'New Job Opening': 'Nieuwe vacature',
+	
+	'New sponsor': 'Nieuwe sponsor',
+	
+	'Theme': 'Thema',
+	'New theme': 'Nieuw thema',
+	
+	'Price': 'Tarief',
+	'New Price': 'Nieuw Tarief',
+	'Order': 'Volgorde',
+	
+	'EducationalProgram': 'Educatie: programma',
+	'New EducationalProgram': 'Nieuw programma',
+	'Summary': 'Samenvatting',
+	
+	'EducationalAudience': 'Educatie: doelgroep',
+	'New EducationalAudience': 'Nieuwe doelgroep',
+	
+	'YouTubeVideo': 'YouTube video',
+	'New YouTube video': 'Nieuwe YouTube video',
+	
+	'New Video': 'Nieuwe video',
+	'YouTube and Vimeo Url\'s are supported' :'YouTube- en Vimeo-video\'s worden ondersteund',
+	
+	'BlogCategory': 'Blog-categorie',
+	'New BlogCategory': 'Nieuwe categorie',
+	
+	'BlogPost': 'BlogPost',
+	'New BlogPost': 'Nieuwe post',
+	
+	'Client': 'Klant',
+	'New Client': 'Nieuwe klant',
+	
+	'New Gedetacheerde': 'Nieuwe gedetacheerde',
+	
+	'Team Member': 'Medewerker',
+	'New Team Member': 'Nieuwe medewerker',
+	
+	'Service': 'Dienst',
+	'New Service': 'Nieuwe dienst',
+	
+	'AgeGroup': 'Leeftijdsgroep',
+	'New AgeGroup': 'Nieuwe leeftijdsgroep',
+	'Page title': 'Paginatitel',
+	'Course': 'Cursus',
+	'New Course': 'Nieuwe cursus',
+	
+	'Contestant': 'Prijsvraagdeelnemer',
+	'New Contestant': 'Nieuwe deelnemer',
+	'BusContest': 'Bus Prijsvraag',
+	'New BusContest': 'Nieuwe prijsvraag',
+	'Text': 'Tekst',
+	'Round': 'Ronde',
+	
+	'New News': 'Nieuw nieuwsitem',
+	'Is bus news': 'Is bus nieuws?',
+	
+	'BusContestDate': 'Eventdatum',
+	'New BusContestDate': 'Nieuwe eventdatum',
+	'Date': 'Datum',
+	'School Name': 'School Naam',
+	'Location': 'Lokatie',
+	'School Address': 'School Adres',
+	'School Zip': 'School Postcode',
+	'School Type': 'School Type',
+	'Class': 'Klas',
+	'Teacher Name': 'Docent Naam',
+	'Prefix': 'Voorvoegsel',
+	'First name': 'Voornaam',
+	'Last name prefix': 'Tussenvoegsel',
+	'Last name': 'Achternaam',
+	'Role': 'Rol',
+	'Is employee': 'Is werknemer',
+	'Teacher Email': 'Docent Email',
+	'Phone': 'Telefoon',
+	'Teacher Branch': 'Docent Vak',
+	'Images': 'Afbeeldingen',
+	'Youtube video URL': 'YouTube Video URL',
+	'Is Subscriber': 'Email-nieuwsbrief',
+	'Date preference': 'Datumvoorkeur',
+	
+	'OrganizationActivity': 'Organisatie-activiteit',
+	
+	'Background': 'Achtergrond',
+	
+	'ScienceQuiz': 'Science Quiz',
+	'Intro title': 'Introductietitel',
+	'Intro text': 'Introductietekst',
+	'Choice': 'Optie',
+	'Answer intro': 'Inleiding op antwoord',
+	'Online': 'Online',
+	'Take offline': 'Offline halen',
+	'Published': 'Publicatie',
+	'Publish': 'Publiceren',
+	'Publish at': 'Inplannen',
+	'Scheduled': 'Ingepland',
+	'Today at' : 'Vandaag om',
+	'Yesterday at' : 'Gisteren om',
+	'No date specified' : 'Geen datum opgegeven',
+	
+	'Scientist': 'Wetenschapper',
+	'New Scientist': 'Nieuwe wetenschapper',
+	'Is presenter?': 'Is presentator?',
+	'Research question': 'Onderzoeksvraag',
+	
+	'WinningSchool': 'Winnende school',
+	'School type': 'School type',
+	'City': 'Stad',
+	
+	'Category': 'Categorie',
+	'New Catgeory': 'Nieuwe Categorie',
+	
+	'Exhibitor':'Deelnemer',
+	'New Exhibitor': 'Nieuwe Deelnemer',
+	'Location X': 'Lokatie X',
+	'Location Y': 'Lokatie Y',
+	'Address' :'Adres',
+	'Zipcode':'Postcode',
+	'Country' :'Land',
+	'Telephone':'Telefoon',
+	'Telephone during tefaf': 'Telefoon gedurende Tefaf',
+	'Use the same': 'Gebruik zelfde',
+	'Venue name': 'Naam venue',
+	'Venue address': 'Adres venue',
+	'Venue city': 'Plaats venue',
+	
+	'FestivalEvent' : 'Festival-evenement',
+	'New FestivalEvent' :'Nieuw Festival-evenement',
+	
+	'Virtual Tour': 'Virtual Tour',
+	'New Virtual Tour' : 'Nieuwe Virtual Tour',
+	
+	'Image small' : 'Afbeelding klein',
+	'Image thumbnail' : 'Afbeelding thumbnail',
+	'Video thumb': 'Video thumbnail',
+	'Color': 'Kleurcode',
+	'Section' : 'Section',
+	'New Section': 'Nieuwe Section',
+	'Short Name': 'Korte naam',
+	
+	'SystemValue':'Parameter',
+	'New SystemValue':'Nieuwe Parameter',
+	'Key': 'Naam',
+	'Value':'Waarde',
+	
+	'New Tweet' : 'Nieuwe Tweet',
+	'Message' : 'Bericht',
+	'Is highlighted' : 'Is Highlight',
+	'Author' : 'Auteur',
+	'Modifier' : 'Bewerkt door',
+	
+	'Is sponsored' : 'Is gesponsord',
+	
+	'English name' : 'Naam in Engels',
+	'Caption' : 'Bijschrift',
+	
+	'Video set': 'Videoset',
+	'New Video set': 'Nieuwe videoset',
+	
+	'Photo set': 'Fotoset',
+	'New Photo set': 'Nieuwe fotoset',
+	
+	'New Post': 'Nieuwe Post',
+
+	'GarpRegistry': 'Bedrijfsprofiel'
+};
