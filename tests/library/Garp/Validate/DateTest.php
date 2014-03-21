49dd76c0b308e2447245dcc4ad92e2e21a475003
Harmen Janssen <harmen@grrr.nl>
2014-03-21 12:49:12 +0100

    Added unit tests to Date validator

--- tests/library/Garp/Validate/DateTest.php
+++ tests/library/Garp/Validate/DateTest.php
@@ -6,22 +6,30 @@
  * @author       Harmen Janssen | grrr.nl
  * @version      0.1.0
  * @package      Garp
- * @group        Validate
+ * @group Validate
  */
 class Garp_Validate_DateTest extends Garp_Test_PHPUnit_TestCase {
 		
-	public function formatsShouldMatch() {
+	public function testFormatsShouldMatch() {
+		/**
+ 	 	 * Test a shitload of date formats
+ 	 	 */
 		$dateFormats = array(
 			array('d-m-Y', '11-02-1985'),
 			array('d-m-Y', '01-10-2012'),
 			array('d-m-Y', '11-10-894'),
-			array('D j M y', 'Mon 1 Jan 85')
+			array('D j M y', 'Mon 1 Jan 85'),
+			array('F \t\h\e jS', 'December the 24th'),
+			array('\W\e\e\k W', 'Week 20'),
+			array('F FF F', 'March JanuaryMay June'), // such a crazy example
+			array('D j/n/y', 'Wed 30/1/94'),
+			array('l d F', 'Saturday 22 October')
 		);
 		foreach ($dateFormats as $i => $f) {
 			$format = $f[0];
 			$match = $f[1];
 			$val = new Garp_Validate_Date($format);
-			$this->assertTrue($val->isValid($match));
+			$this->assertTrue($val->isValid($match), "$match does not match date format $format");
 		}
 	}		
 
