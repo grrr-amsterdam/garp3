51c0b25f090b86351e18baa0e63f1a835a7768b6
Larix Kortbeek <larixk@gmail.com>
2014-04-11 15:56:34 +0200

    image and video picker

---
+++ public/js/ckeditor/plugins/garpimages/plugin.js
@@ -0,0 +1,41 @@
+CKEDITOR.plugins.add('garpimages', {
+	init: function(editor) {
+		editor.addCommand('garpimageDialog', {
+			exec: function(editor) {
+				var command = this;
+
+				// Open Extjs image picker
+				var win = new Garp.ImagePickerWindow({});
+				win.on('select', function(data) {
+					// Insert image element
+					var tpl = Garp.imageTpl;
+
+					var imgHtml = tpl.apply({
+						path: data.src,
+						width: data.template.get('w'),
+						height: data.template.get('h'),
+						align: data.align,
+						caption: data.caption || false
+					});
+					console.log(imgHtml);
+					var element = CKEDITOR.dom.element.createFromHtml(imgHtml);
+					editor.insertElement(element);
+
+					// Return to CKEditor
+					editor.fire('afterCommandExec', {
+						name: command.name,
+						command: command
+					});
+				}, this);
+				win.show();
+
+			},
+			async: true
+		});
+		editor.ui.addButton('Garpimage', {
+			label: 'Insert Image',
+			command: 'garpimageDialog',
+			toolbar: 'insert'
+		});
+	}
+});
