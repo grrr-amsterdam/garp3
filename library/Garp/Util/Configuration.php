288f49e7ac430d31a959aeb77f0fb0fb220ae1fc
Harmen Janssen <harmen@grrr.nl>
2014-05-13 08:08:23 +0200

    Cleaner use of constants

--- library/Garp/Util/Configuration.php
+++ library/Garp/Util/Configuration.php
@@ -2,14 +2,29 @@
 /**
  * Garp_Util_Configuration
  * A configuration array with some convenience methods built-in
- * @author Harmen Janssen | grrr.nl
- * @modifiedby $LastChangedBy: $
- * @version $Revision: $
- * @package Garp
- * @subpackage Util
- * @lastmodified $Date: $
+ *
+ * @author       Harmen Janssen | grrr.nl
+ * @version      0.1.5
+ * @package      Garp_Util
  */
 class Garp_Util_Configuration extends ArrayIterator {
+	/**#@+
+ 	 * Constants used in type validation
+ 	 */
+	const TYPE_ARRAY   = 'array';
+	const TYPE_OBJECT  = 'object';
+	const TYPE_NUMBER  = 'number';
+	const TYPE_NUMERIC = 'numeric';
+	const TYPE_STRING  = 'string';
+    /**#@-*/
+
+	/**#@+
+ 	 * Exceptions
+ 	 */
+	const EXCEPTION_MISSINGKEY = '"%s" is required but not set.';
+	const EXCEPTION_INCORRECTTYPE = '"%s" is set but not the correct type.';
+	/**#@-*/
+
 	/**
 	 * Convenience method for requiring a certain key.
 	 * (note; this method is not called "require" as would be 
@@ -20,7 +35,7 @@ class Garp_Util_Configuration extends ArrayIterator {
 	 * @throws Garp_Util_Configuration_Exception
 	 */
 	public function obligate($key, $msg = '') {
-		$msg = $msg ?: "\"$key\" is required but not set.";
+		$msg = $msg ?: sprintf(self::EXCEPTION_MISSINGKEY, $key);
 		if (!$this->offsetExists($key)) {
 			throw new Garp_Util_Configuration_Exception($msg);
 		}
@@ -45,20 +60,28 @@ class Garp_Util_Configuration extends ArrayIterator {
 		return $this;
 	}
 
-	public function obligateType($key, $type, $msg = '') {
+	/**
+ 	 * Validate a value's type
+ 	 * @param String $key
+ 	 * @param String $type
+ 	 * @param String $msg Optional error message
+ 	 * @return Garp_Util_Configuration $this
+ 	 * @throws Garp_Util_Configuration_Exception_IncorrectType
+ 	 */
+	public function validateType($key, $type, $msg = '') {
 		$this->obligate($key);
 		$val = $this[$key];
-		$msg = $msg ?: "\"$key\" is set but not the correct type.";
+		$msg = $msg ?: sprintf(self::EXCEPTION_INCORRECTTYPE, $key); 
 		$valid = true;
-		if ($type === 'array') {
+		if ($type === self::TYPE_ARRAY) {
 			$valid = is_array($val);
-		} elseif ($type === 'object') {
+		} elseif ($type === self::TYPE_OBJECT) {
 			$valid = is_object($val);
-		} elseif ($type === 'number') {
+		} elseif ($type === self::TYPE_NUMBER) {
 			$valid = is_int($val) || is_float($val);
-		} elseif ($type === 'numeric') {
+		} elseif ($type === self::TYPE_NUMERIC) {
 			$valid = is_numeric($val);
-		} elseif ($type === 'string') {
+		} elseif ($type === self::TYPE_STRING) {
 			$valid = is_string($val);
 		}
 
