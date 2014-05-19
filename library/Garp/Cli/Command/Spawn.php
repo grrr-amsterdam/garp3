f0454e70faafaa54ecb605f3a50d50c48fb723f8
David Spreekmeester <david@grrr.nl>
2014-05-19 16:22:21 +0200

    working on non-interactive Spawn

--- library/Garp/Cli/Command/Spawn.php
+++ library/Garp/Cli/Command/Spawn.php
@@ -9,32 +9,10 @@
  * @lastmodified $Date: $
  */
 class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
-	/**
-	 * The command to dump a specific Javascript base model - since this
-	 * is normally concatenated and minified with the other base models.
-	 */
-	const JS_BASE_MODEL_COMMAND = 'showJsBaseModel';
-	
-	/**
-	 * The argument used to spawn only the database, or only files.
-	 */
-	const FILTER_MODULE_COMMAND = 'only';
-	const FILTER_MODULE_PHP 	= 'php';
-	const FILTER_MODULE_DB 		= 'db';
-	const FILTER_MODULE_JS 		= 'js';
-	const FILTER_MODULE_FILES 	= 'files';
-	
+
 	const ERROR_UNKNOWN_ARGUMENT = 
 		"Sorry, I do not know the '%s' argument. Try 'garp spawn help' for an overview of options.";	
-	const ERROR_ILLEGAL_MODULE_FILTER = 
-		"Sorry, '%s' is not a valid value for the '--only' parameter. Try 'garp spawn help' for an overview of options.";
 
-
-	/**
-	 * @var Array $_allowedFilters
-	 */
-	protected $_allowedFilters = array('files', 'db', 'js', 'php');
-	
 	/**
 	 * @var Garp_Spawn_Model_Set $_modelSet
 	 */
@@ -44,6 +22,16 @@ class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
 	 * @var Array $_args The arguments given when calling this command
 	 */
 	protected $_args;	
+
+	/**
+ 	 * @var Garp_Cli_Command_Spawn_Filter $_filter
+ 	 */
+	protected $_filter;
+
+	/**
+ 	 * @var Garp_Cli_Command_Spawn_Feedback $_feedback
+ 	 */
+	protected $_feedback;
 
 
 	/**
@@ -57,30 +45,17 @@ class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
 		}
 
 		$this->setArgs($args);
-
+		$this->setFilter(new Garp_Cli_Command_Spawn_Filter($args));
+		$this->setFeedback(new Garp_Cli_Command_Spawn_Feedback($args));
 		$this->setModelSet($this->_initModelSet());
 
-		array_key_exists(self::JS_BASE_MODEL_COMMAND, $args) ?
-			$this->_showJsBaseModel($args[self::JS_BASE_MODEL_COMMAND]) :
-			$this->_spawn()
+		$feedback = $this->getFeedback();
+		$feedback->shouldSpawn()
+			? $this->_spawn()
+			: $this->_showJsBaseModel($args[Garp_Cli_Command_Spawn_Feedback::JS_BASE_MODEL_COMMAND])
 		;
 	}
 
-	/**
-	 * Returns the module that should be run, i.e. 'db' or 'files', or null if no filter is given.
-	 */
-	public function getModuleFilter() {
-		$args 	= $this->getArgs();
-		$only	= self::FILTER_MODULE_COMMAND;
-		
-		if (!array_key_exists($only, $args)) {
-			return;
-		}
-
-		$filter = $args[$only];		
-		return strtolower($filter);
-	}
-	
 	/**
 	 * @return Array
 	 */
@@ -96,19 +71,27 @@ class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
 		$this->_args = $args;
 	}
 	
-	/**
-	 * @return Array
-	 */
-	public function getAllowedFilters() {
-		return $this->_allowedFilters;
+	public function setFilter(Garp_Cli_Command_Spawn_Filter $filter) {
+		$this->_filter = $filter;
 	}
-	
+
 	/**
-	 * @param Array $allowedFilters
-	 */
-	public function setAllowedFilters($allowedFilters) {
-		$this->_allowedFilters = $allowedFilters;
-	}	
+ 	 * @return Garp_Cli_Command_Spawn_Filter
+ 	 */
+	public function getFilter() {
+		return $this->_filter;
+	}
+
+	public function setFeedback(Garp_Cli_Command_Spawn_Feedback $feedback) {
+		$this->_feedback = $feedback;
+	}
+
+	/**
+ 	 * @return Garp_Cli_Command_Spawn_Feedback
+ 	 */
+	public function getFeedback() {
+		return $this->_feedback;
+	}
 	
 	/**
 	 * @return Garp_Spawn_Model_Set
@@ -134,16 +117,20 @@ class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
 	 * Spawn JS and PHP files and the database structure.
 	 */
 	protected function _spawn() {
-		$dbShouldSpawn  		= $this->_shouldSpawnDb();
-		$jsShouldSpawn			= $this->_shouldSpawnJs();
-		$phpShouldSpawn			= $this->_shouldSpawnPhp();
+		$filter = $this->getFilter();
+
+		$dbShouldSpawn  		= $filter->shouldSpawnDb();
+		$jsShouldSpawn			= $filter->shouldSpawnJs();
+		$phpShouldSpawn			= $filter->shouldSpawnPhp();
 		$someFilesShouldSpawn	= $jsShouldSpawn || $phpShouldSpawn;
 
 		if ($someFilesShouldSpawn) {
 			$this->_spawnFiles();
 		}
-		
-		Garp_Cli::lineOut("\n");
+
+		if ($this->getFeedback()->isInteractive()) {
+			Garp_Cli::lineOut("\n");
+		}
 		
 		if ($dbShouldSpawn) {
 			$this->_spawnDb();
@@ -153,30 +140,43 @@ class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
 	protected function _spawnDb() {
 		$modelSet = $this->getModelSet();
 
-		Garp_Cli::lineOut("\nDatabase");
+		if ($this->getFeedback()->isInteractive()) {
+			Garp_Cli::lineOut("\nDatabase");
+		}
 
 		$dbManager = Garp_Spawn_MySql_Manager::getInstance();
 		$dbManager->run($modelSet);
 		
-		Garp_Cli::lineOut("\n");
+		if ($this->getFeedback()->isInteractive()) {
+			Garp_Cli::lineOut("\n");
+		}
 
 		$cacheDir = $this->_getCacheDir();
 		Garp_Cache_Manager::purgeStaticCache(null, $cacheDir);
 		Garp_Cache_Manager::purgeMemcachedCache(null);
-		Garp_Cli::lineOut("All cache purged.");
+
+		if ($this->getFeedback()->isInteractive()) {
+			Garp_Cli::lineOut("All cache purged.");
+		}
 	}
 	
 	protected function _spawnFiles() {
 		$modelSet 		= $this->getModelSet();
 
 		$totalActions 	= $this->_calculateTotalFileActions();
-		$jsShouldSpawn	= $this->_shouldSpawnJs();
-		$phpShouldSpawn	= $this->_shouldSpawnPhp();
+		$jsShouldSpawn	= $this->getFilter()->shouldSpawnJs();
+		$phpShouldSpawn	= $this->getFilter()->shouldSpawnPhp();
 
-		$progress 		= Garp_Cli_Ui_ProgressBar::getInstance();
+		$progress 		= $this->getFeedback()->isInteractive()
+			? Garp_Cli_Ui_ProgressBar::getInstance()
+			: Garp_Cli_Ui_BatchOutput::getInstance()
+		;
+
 		$progress->init($totalActions);
-		
-		Garp_Cli::lineOut("\nFiles");
+			
+		if ($this->getFeedback()->isInteractive()) {
+			Garp_Cli::lineOut("\nFiles");
+		}
 
 		if ($jsShouldSpawn) {
 			$progress->display("Cooking up base model goo.");
@@ -207,8 +207,8 @@ class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
 	
 	protected function _calculateTotalFileActions() {
 		$modelSet 			= $this->getModelSet();
-		$jsShouldSpawn		= $this->_shouldSpawnJs();
-		$phpShouldSpawn		= $this->_shouldSpawnPhp();
+		$jsShouldSpawn		= $this->getFilter()->shouldSpawnJs();
+		$phpShouldSpawn		= $this->getFilter()->shouldSpawnPhp();
 
 		$modelCount 		= count($modelSet);
 		$multiplier			= $jsShouldSpawn + $phpShouldSpawn;
@@ -219,33 +219,6 @@ class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
 		return $totalActions;
 	}
 
-	protected function _shouldSpawnDb() {
-		$filter = $this->getModuleFilter();
-		
-		return 
-			$filter !== self::FILTER_MODULE_FILES &&
-			$filter !== self::FILTER_MODULE_PHP &&
-			$filter !== self::FILTER_MODULE_JS
-		;
-	}
-
-	protected function _shouldSpawnPhp() {
-		$filter = $this->getModuleFilter();
-
-		return 
-			$filter !== self::FILTER_MODULE_DB &&
-			$filter !== self::FILTER_MODULE_JS
-		;
-	}
-
-	protected function _shouldSpawnJs() {
-		$filter = $this->getModuleFilter();
-
-		return 
-			$filter !== self::FILTER_MODULE_DB &&
-			$filter !== self::FILTER_MODULE_PHP
-		;
-	}
 
 	protected function _showJsBaseModel($modelId) {
 		$modelSet = $this->getModelSet();
@@ -283,51 +256,36 @@ class Garp_Cli_Command_Spawn extends Garp_Cli_Command {
 		$error = sprintf(self::ERROR_UNKNOWN_ARGUMENT, $args[0]);
 		Garp_Cli::errorOut($error);
 		exit;
-		
-		$this->_validateModuleFilterArgument($args);
 	}
 	
-	/**
-	 * 	Check if an allowed '--only=xx' command is called.
-	 */
-	protected function _validateModuleFilterArgument(array $args) {
-		$only 			= self::FILTER_MODULE_COMMAND;
-		$allowedFilters = $this->getAllowedFilters();
-		
-		if (
-			array_key_exists($only, $args) &&
-			$filter	= strtolower($args[$only]) &&
-			!in_array($filter, $allowedFilters)
-		) {
-			$error = sprintf(self::ERROR_ILLEGAL_MODULE_FILTER, $args[$only]);
-			Garp_Cli::errorOut($error);
-			exit;
-		}
-	}
 
 	protected function _displayHelp() {
 		$lines = array(
-			"\n",
+			"",
+			"• Batch",
+			"garp spawn -b",
+			"\tRun in batch mode (non-interactive)",
+			"",
 			"• Filtering",
 			"garp spawn --only=files",
 			"\tOnly Spawn files, skip the database",
-			"\n",
+			"",
 			"garp spawn --only=js",
 			"\tOnly Spawn Javascript files,",
 			"\tskip PHP files and the database",
-			"\n",
+			"",
 			"garp spawn --only=php",
 			"\tOnly Spawn PHP files,",
 			"\tskip Javascript files and the database",
-			"\n",
+			"",
 			"garp spawn --only=db",
 			"\tOnly Spawn database,",
 			"\tskip file generation",
-			"\n",
-			"\n• Debugging",
+			"",
+			"• Debugging",
 			"garp spawn --showJsBaseModel=YourModel",
 			"\tShow the non-minified JS base model.",
-			"\n",
+			"",
 			"garp spawn --showJsBaseModel=YourModel > YourFile.json",
 			"\tWrite the non-minified JS base model to a file."
 		);
---
+++ library/Garp/Cli/Command/Spawn/Filter.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Garp_Cli_Command_Spawn_Filter
+ * @author David Spreekmeester | grrr.nl
+ * @modifiedby $LastChangedBy: $
+ * @version $Revision: $
+ * @package Garp
+ * @subpackage Cli
+ * @lastmodified $Date: $
+ */
+class Garp_Cli_Command_Spawn_Filter {
+	/**
+	 * The argument used to spawn only the database, or only files.
+	 */
+	const FILTER_MODULE_COMMAND = 'only';
+	const FILTER_MODULE_PHP 	= 'php';
+	const FILTER_MODULE_DB 		= 'db';
+	const FILTER_MODULE_JS 		= 'js';
+	const FILTER_MODULE_FILES 	= 'files';
+
+	const ERROR_ILLEGAL_MODULE_FILTER = 
+		"Sorry, '%s' is not a valid value for the '--only' parameter. Try 'garp spawn help' for an overview of options.";
+
+	/**
+	 * @var Array $_allowedFilters
+	 */
+	protected $_allowedFilters = array('files', 'db', 'js', 'php');
+
+	/**
+ 	 * @var Array $_args
+ 	 */
+	protected $_args;
+
+
+	/**
+ 	 * @param Array $args Array of commandline arguments provided
+ 	 */
+	public function __construct(Array $args) {
+		$this->_validateModuleFilterArgument($args);
+		$this->setArgs($args);
+	}
+
+	/**
+	 * @return Array
+	 */
+	public function getAllowedFilters() {
+		return $this->_allowedFilters;
+	}
+	
+	/**
+	 * @param Array $allowedFilters
+	 */
+	public function setAllowedFilters($allowedFilters) {
+		$this->_allowedFilters = $allowedFilters;
+	}	
+
+	public function getArgs() {
+		return $this->_args;
+	}
+
+	public function setArgs(array $args) {
+		$this->_args = $args;
+	}
+
+	public function shouldSpawnDb() {
+		$filter = $this->_getModuleFilter();
+		
+		return 
+			$filter !== self::FILTER_MODULE_FILES &&
+			$filter !== self::FILTER_MODULE_PHP &&
+			$filter !== self::FILTER_MODULE_JS
+		;
+	}
+
+	public function shouldSpawnPhp() {
+		$filter = $this->_getModuleFilter();
+
+		return 
+			$filter !== self::FILTER_MODULE_DB &&
+			$filter !== self::FILTER_MODULE_JS
+		;
+	}
+
+	public function shouldSpawnJs() {
+		$filter = $this->_getModuleFilter();
+
+		return 
+			$filter !== self::FILTER_MODULE_DB &&
+			$filter !== self::FILTER_MODULE_PHP
+		;
+	}
+	/**
+	 * Returns the module that should be run, i.e. 'db' or 'files', or null if no filter is given.
+	 */
+	protected function _getModuleFilter() {
+		$args 	= $this->getArgs();
+		$only	= self::FILTER_MODULE_COMMAND;
+		
+		if (!array_key_exists($only, $args)) {
+			return;
+		}
+
+		$filter = $args[$only];		
+		return strtolower($filter);
+	}
+
+	/**
+	 * 	Check if an allowed '--only=xx' command is called.
+	 */
+	protected function _validateModuleFilterArgument(array $args) {
+		$only 			= self::FILTER_MODULE_COMMAND;
+		$allowedFilters = $this->getAllowedFilters();
+		
+		if (
+			array_key_exists($only, $args) &&
+			$filter	= strtolower($args[$only]) &&
+			!in_array($filter, $allowedFilters)
+		) {
+			$error = sprintf(self::ERROR_ILLEGAL_MODULE_FILTER, $args[$only]);
+			Garp_Cli::errorOut($error);
+			exit;
+		}
+	}
+}
