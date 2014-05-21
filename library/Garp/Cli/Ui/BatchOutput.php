aab1bec3fe6ed5e79973bbf7c485dac1eff01564
David Spreekmeester <david@grrr.nl>
2014-05-21 16:44:23 +0200

    Finished up non-interactive Spawn

--- library/Garp/Cli/Ui/BatchOutput.php
+++ library/Garp/Cli/Ui/BatchOutput.php
@@ -3,9 +3,17 @@
  * Provides methods to display progress in a batch (non-interactive) context.
  * @author David Spreekmeester | Grrr.nl
  */
-class Garp_Cli_Ui_BatchOutput {
-	public function display($string) {
-	  return Garp_Cli::lineOut($string);
+class Garp_Cli_Ui_BatchOutput extends Garp_Cli_Ui {
+	public function display($message = null, $itemsLeftMessage = null) {
+		echo $message . '. ';
+	}
+
+	public function displayHeader($string) {
+		echo $string . ': ';
+	}
+
+	public function displayError($string) {
+		return Garp_Cli::errorOut($string);
 	}
 
 }
