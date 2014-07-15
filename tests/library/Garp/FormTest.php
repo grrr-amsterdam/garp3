dcce8c771648e2f505bb118b4d04d5d29acf2c95
Iosif Vigh <iosif@grrr.nl>
2014-07-15 15:40:37 +0200

    Garp_Form isValid testing

---
+++ tests/library/Garp/FormTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @group Form
+ */
+class Garp_FormTest extends Garp_Test_PHPUnit_TestCase {
+
+	public function testFormWithDashes(){
+
+		$form = new Garp_Form();
+		$form->setAction('/my/action/')
+		     ->setMethod('post');
+		$form->addElement('email', 'e-mail', array(
+		    'label' => 'Your email address',
+		    'required' => true
+		));
+		$form->addElement('password', 'pass-word',array(
+			'label' => 'the label',
+			'required' => true
+			));
+		$form->addElement('submit', 'Register');
+
+		$this->assertTrue($form->isValid(array(
+			'e-mail' => 'iosif@grrr.nl',
+			'pass-word' => '123456'
+		)));
+
+	}
+}
