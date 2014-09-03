9093badcc203515359411b87e65b03af7cbb5e87
David Spreekmeester <david@grrr.nl>
2014-09-03 15:55:49 +0200

    Added Capistrano 3 configuration (from v2)

---
+++ application/configs/deploy.rb
@@ -0,0 +1,13 @@
+set :log_level, :info
+set :linked_dirs, %w{
+	public/uploads/documents 
+	public/uploads/images 
+	application/data/logs 
+	application/data/cache/tags
+}
+set :keep_releases, 3
+
+load "application/configs/deploy.rb"
+
+Dir.glob("garp/deploy/tasks/*.cap").each { |r| load r }
+load "garp/deploy/garp3.cap"
