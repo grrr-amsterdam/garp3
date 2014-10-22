ebb1860d941418af86475d4301b57a8c0d272b47
Harmen Janssen <harmen@grrr.nl>
2014-10-22 14:41:59 +0200

    Store user image from Facebook user

--- application/modules/g/models/Image.php
+++ application/modules/g/models/Image.php
@@ -32,17 +32,7 @@ class G_Model_Image extends Model_Base_Image {
 		// @todo file_get_contents to optimistic?
 		$bytes = file_get_contents($imageUrl);
 		if (is_null($filename)) {
-			$filename = basename($imageUrl);
-			if (strpos($filename, '.') === false) {
-				$mime = $this->_getImageMime($bytes);
-				if ($mime === 'application/x-gzip') {
-					$bytes = gzdecode($bytes);
-					$mime = $this->_getImageMime($bytes);
-				}
-				
-				// Figure out mimetype, or default to jpg
-				$filename .= '.' . (new Garp_File_Extension($mime) ?: 'jpg');
-			}
+			$filename = $this->_createFilenameFromUrl($imageUrl);
 		}
 		$response = Zend_Controller_Action_HelperBroker::getStaticHelper('upload')
 			->uploadRaw(Garp_File::TYPE_IMAGES, $filename, $bytes);
@@ -58,4 +48,27 @@ class G_Model_Image extends Model_Base_Image {
 		$mime = $mime[0];
 		return $mime;
 	}		
+
+	protected function _createFilenameFromUrl($imageUrl) {
+		$filename = basename($imageUrl);
+		// Strip possible query parameters
+		if (strpos($filename, '?') !== false &&
+			strpos($filename, '.') !== false &&
+			strpos($filename, '?') > strrpos($filename, '.')) {
+			// Extract everything up until the "?"
+			$filename = substr($filename, 0, strrpos($filename, '?'));
+		}
+		// Append extension based on mime-type
+		if (strpos($filename, '.') === false) {
+			$mime = $this->_getImageMime($bytes);
+			if ($mime === 'application/x-gzip') {
+				$bytes = gzdecode($bytes);
+				$mime = $this->_getImageMime($bytes);
+			}
+
+			// Figure out mimetype, or default to jpg
+			$filename .= '.' . (new Garp_File_Extension($mime) ?: 'jpg');
+		}
+		return $filename;
+	}
 }
