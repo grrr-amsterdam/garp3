52cc492f0600a472bd227ac4ccc6abc4f4d2d5ba
Harmen Janssen <harmen@grrr.nl>
2014-10-24 16:32:06 +0200

    Added throwing of exceptions to ControllerTestCase

--- library/Garp/Test/PHPUnit/ControllerTestCase.php
+++ library/Garp/Test/PHPUnit/ControllerTestCase.php
@@ -11,6 +11,27 @@ abstract class Garp_Test_PHPUnit_ControllerTestCase extends Zend_Test_PHPUnit_Co
 		$this->_helper = new Garp_Test_PHPUnit_Helper();
 		parent::__construct();
 	}
+
+	/**
+ 	 * Overwritten to toss exceptions in your face, so developers don't have to inspect the 
+ 	 * HTML response to see what went wrong.
+ 	 * We'll skip Zend_Controller_Action_Exceptions though, because they actually provide semantic 
+ 	 * meaning to response. For instance, you can use them to adjust the HTTP status code, which is 
+ 	 * actually a valid response to these exception.
+ 	 * This method should throw only unexpected exceptions that need fixing right away.
+ 	 */
+    public function dispatch($url = null) {
+		$response = parent::dispatch($url);
+		if (!$this->getResponse()->isException()) {
+			return $response;
+		}
+		foreach ($this->getResponse()->getException() as $exp) {
+			if (!$exp instanceof Zend_Controller_Action_Exception) {
+				throw $exp;
+			}
+		}
+		return $response;
+	}
 	
     public function setUp() {
 		$this->application = new Garp_Application(
