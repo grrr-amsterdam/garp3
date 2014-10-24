3ad322fec2a9b4f2f31c896f99d13df66a32403a
Koen Schaft <koen@grrr.nl>
2014-10-24 13:45:40 +0200

    Added custom file input; Re-arranged form inputs;

--- library/Garp/Form/Element/File.php
+++ library/Garp/Form/Element/File.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Garp_Form_Element_File
- * 
- * ¡NOTE! This element is a lot dumber than Zend_Form_Element_File. It will not 
- * upload the file, it will not validate $_FILES. It is just a simple text input 
- * with type="file". This is by design, since we control what happens with files 
- * either in the controller or with an AJAX upload script controlled from 
+ *
+ * ¡NOTE! This element is a lot dumber than Zend_Form_Element_File. It will not
+ * upload the file, it will not validate $_FILES. It is just a simple text input
+ * with type="file". This is by design, since we control what happens with files
+ * either in the controller or with an AJAX upload script controlled from
  * garp.front.js.
  *
  * @author Harmen Janssen | grrr.nl
@@ -18,8 +18,8 @@ class Garp_Form_Element_File extends Zend_Form_Element_Xhtml {
      * @var string Default view helper
      */
     public $helper = 'formFile';
-	
-	
+
+
 	public function init() {
 		// parentClass might by set by Garp_Form
 		$parentClass = null;
@@ -41,7 +41,7 @@ class Garp_Form_Element_File extends Zend_Form_Element_Xhtml {
 			$uploadInfoStr = $this->_getUploadInfoString();
 			$options['description'] = $uploadInfoStr;
 		}
-		$this->setOptions($options);	
+		$this->setOptions($options);
 
 		// Render file metadata as data-attributes so Javascript can use them.
 		if (!$this->getAttrib('data-max-file-size')) {
@@ -49,10 +49,10 @@ class Garp_Form_Element_File extends Zend_Form_Element_Xhtml {
 		}
 		if (!$this->getAttrib('data-allowed-extensions')) {
 			$extensions = implode(',', $this->getAllowedExtensions());
-			$this->setAttrib('data-allowed-extensions', $extensions);		
+			$this->setAttrib('data-allowed-extensions', $extensions);
 		}
 	}
-	
+
 
 	/**
  	 * Retrieve an informative string describing upload restrictions.
@@ -65,9 +65,9 @@ class Garp_Form_Element_File extends Zend_Form_Element_Xhtml {
 
 		$translator = $this->getTranslator();
 		$uploadInfoStr = $translator->translate('Only %1$s and %2$s files with a maximum of %3$s MB are allowed');
-		$uploadInfoStr = sprintf($uploadInfoStr, implode(', ', $allowedExtensions), $lastExtension, $maxFileSize); 
-		
-		return $uploadInfoStr;		
+		$uploadInfoStr = sprintf($uploadInfoStr, implode(', ', $allowedExtensions), $lastExtension, $maxFileSize);
+
+		return $uploadInfoStr;
 	}
 
 
