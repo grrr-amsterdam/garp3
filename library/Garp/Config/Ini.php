89c99c9d4b98e2e5e129c124ca3df3b4be7f4d9d
Harmen Janssen <harmen@grrr.nl>
2015-01-02 10:59:24 +0100

    Removed Garp_Cache_Ini

--- library/Garp/Config/Ini.php
+++ library/Garp/Config/Ini.php
@@ -11,6 +11,35 @@
  */
 class Garp_Config_Ini extends Zend_Config_Ini {
 
+	/**
+ 	 * Keep track of loaded ini files within a single request
+ 	 * @var Array
+ 	 */
+	protected static $_store = array();
+
+	/**
+ 	 * Receive a config ini file from cache
+ 	 * @param String $filename
+ 	 * @return Garp_Config_Ini
+ 	 */
+	public static function getCached($filename) {
+		$cache = Zend_Registry::get('CacheFrontend');
+		$key = preg_replace('/[^0-9a-zA-Z_]/', '_', basename($filename));
+
+		// Check the store first to see if the ini file is already loaded within this session
+		if (array_key_exists($key, static::$_store)) {
+			return static::$_store[$key];
+		}
+
+		$config = $cache->load('Ini_Config_'.$key);
+		if (!$config) {
+			$config = new Garp_Config_Ini($filename, APPLICATION_ENV);
+			$cache->save($config, 'Ini_Config_'.$key);
+		}
+		static::$_store[$key] = $config;
+		return $config;
+	}
+
     public function __construct($filename, $section = null, $options = false) {
 		$options['allowModifications'] = true;
 		parent::__construct($filename, $section, $options);
@@ -41,7 +70,7 @@ class Garp_Config_Ini extends Zend_Config_Ini {
 	}
 
 	/**
- 	 * Take an ini string to populate the config object. 
+ 	 * Take an ini string to populate the config object.
  	 * @param String $iniString
  	 * @param String $section
  	 * @param Array $options
