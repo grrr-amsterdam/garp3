79c316682a0680b79a066c462d7cc9f0aea97d3d
Harmen Janssen <harmen@grrr.nl>
2015-01-08 23:27:59 +0100

    Created default Garp auth models

---
+++ public/js/models/AuthLinkedin.js
@@ -0,0 +1,8 @@
+/** EXTENDED MODEL **/
+(function() {
+	if (!('AuthLinkedin' in Garp.dataTypes)) {
+		return;
+	}
+	Garp.dataTypes.AuthLinkedin.on('init', function() {
+	});
+})();
---
+++ public/js/models/AuthTwitter.js
@@ -0,0 +1,8 @@
+/** EXTENDED MODEL **/
+(function() {
+	if (!('AuthTwitter' in Garp.dataTypes)) {
+		return;
+	}
+	Garp.dataTypes.AuthTwitter.on('init', function() {
+	});
+})();
