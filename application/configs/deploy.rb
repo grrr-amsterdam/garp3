137c0ceb7021a92a4700cf31f8dd5bbd3a8b860b
Harmen Janssen <harmen@grrr.nl>
2015-01-30 14:33:07 +0100

    Updated deploy tasks to toggle Under Construction
    Note: this is commented out in this commit, since the current release
    won't feature the right cli command.

--- application/configs/deploy.rb
+++ application/configs/deploy.rb
@@ -1,11 +1,15 @@
 #set :log_level, :info
 set :linked_dirs, %w{
-	public/uploads/documents 
-	public/uploads/images 
-	application/data/logs 
+	public/uploads/documents
+	public/uploads/images
+	application/data/logs
 	application/data/cache/tags
 }
 set :keep_releases, 3
+
+# Grab last release directory, if any
+releases = capture("ls #{File.join(fetch(:deploy_to), 'releases')}")
+set :this_host_last_release = releases.split("\n").sort.last
 
 load "application/configs/deploy.rb"
 
