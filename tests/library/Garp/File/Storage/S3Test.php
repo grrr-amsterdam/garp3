44502889fc610e7d5fac6fd890cbda0f368c2cc3
Harmen Janssen <harmen@grrr.nl>
2015-04-23 17:03:21 +0200

    Added extra test to s3test for sanity

--- tests/library/Garp/File/Storage/S3Test.php
+++ tests/library/Garp/File/Storage/S3Test.php
@@ -14,7 +14,7 @@ class Garp_File_Storage_S3_Test extends Garp_Test_PHPUnit_TestCase {
 
 
 		$testContent =  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-		$this->_storage->store($this->_gzipTestFile, $testContent, true);
+		$this->assertTrue(!!$this->_storage->store($this->_gzipTestFile, $testContent, true));
 
 		$contents = $this->_storage->fetch($this->_gzipTestFile);
 		$this->assertTrue(strlen($contents) > 0);
