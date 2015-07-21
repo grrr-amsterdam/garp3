b450e0c7ea4f58a299b5610c9a98c1c0a68a1f07
Harmen Janssen <harmen@grrr.nl>
2015-07-21 09:51:48 +0200

    Added Garp_Db_Table_Rowset::map

---
+++ application/modules/mocks/models/Thing.php
@@ -0,0 +1,4 @@
+<?php
+class Mocks_Model_Thing extends Garp_Model_Db {
+	protected $_name = '_tests_thing';
+}
--- library/Garp/Db/Table/Rowset.php
+++ library/Garp/Db/Table/Rowset.php
@@ -7,7 +7,7 @@
  * @version      1.0
  * @package      Garp_Db_Table
  */
- 
+
 class Garp_Db_Table_Rowset extends Zend_Db_Table_Rowset_Abstract {
 
 	/**
@@ -23,4 +23,20 @@ class Garp_Db_Table_Rowset extends Zend_Db_Table_Rowset_Abstract {
 		return $out;
 	}
 
+	/**
+ 	 * Project a rowset using $fn
+ 	 * @param Function $fn
+ 	 * @return Garp_Db_Table_Rowset
+ 	 */
+	public function map($fn) {
+		$rows = array_map($fn, $this->toArray());
+		$out = new $this(array(
+            'table'    => $this->getTable(),
+            'data'     => $rows,
+            'readOnly' => $this->_readOnly,
+            'rowClass' => $this->_rowClass,
+            'stored'   => true
+        ));
+		return $out;
+	}
 }
