d5fa5524f350e6f4b36a899516351541dbdc0359
Ramiro Hammen <ramiro.hammen@gmail.com>
2015-09-18 14:00:16 +0200

    better config conditional _renderSelect

--- library/Garp/Spawn/MySql/View/Joint.php
+++ library/Garp/Spawn/MySql/View/Joint.php
@@ -114,12 +114,12 @@ class Garp_Spawn_MySql_View_Joint extends Garp_Spawn_MySql_View_Abstract {
 		}
 
 		$config = Zend_Registry::get('config');
-		if (!($config->spawn && $config->spawn->use_new_joint_view)) {
-			foreach ($singularRelations as $relName => $rel) {
-				$select .= $this->_getJoinStatement($tableName, $relName, $rel);
-			}
+		if ($config->spawn && $config->spawn->use_new_joint_view) {
+			return $select;
 		}
-
+		foreach ($singularRelations as $relName => $rel) {
+			$select .= $this->_getJoinStatement($tableName, $relName, $rel);
+		}
 		return $select;
 	}
 
