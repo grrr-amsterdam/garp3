b849f13aff8cd9058d1ebf7038e536ab82f785db
Harmen Janssen <harmen@grrr.nl>
2015-11-25 11:47:55 +0100

    Made php SCL package configurable

--- library/Garp/Job/Background.php
+++ library/Garp/Job/Background.php
@@ -18,10 +18,14 @@ class Garp_Job_Background {
 	public function __construct($command) {
 		$scriptPath = realpath(APPLICATION_PATH . '/../garp/scripts/garp.php');
 		$phpCmd     = "(php {$scriptPath} {$command} --e=" . APPLICATION_ENV . ' &> /dev/null &)';
+		$sclPhpPackage = 'php54';
+		if (isset(Zend_Registry::get('config')->scl->phpPackage)) {
+			$sclPhpPackage = Zend_Registry::get('config')->scl->phpPackage;
+		}
 
 		// Tests wether SCL is available, and if so executes php thru it.
 		// Otherwise executes php directly.
-		exec("if command -v scl >/dev/null 2>&1; then scl enable php54 \"{$phpCmd}\" ; " .
+		exec("if command -v scl >/dev/null 2>&1; then scl enable {$sclPhpPackage} \"{$phpCmd}\" ; " .
 			"else {$phpCmd}; fi;", $output, $status);
 
 		if (!$this->_commandWasSuccessful($status)) {
