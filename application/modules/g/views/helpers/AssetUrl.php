2b70c5fc44300928d588a14cfb7bfd8f3ae693e5
Harmen Janssen <harmen@grrr.nl>
2016-03-29 08:03:37 +0200

    Fixed hard-coded staging env

--- application/modules/g/views/helpers/AssetUrl.php
+++ application/modules/g/views/helpers/AssetUrl.php
@@ -153,7 +153,7 @@ class G_View_Helper_AssetUrl extends Zend_View_Helper_BaseUrl {
 
 	public function getRevManifest() {
 		if (!$this->_revManifest) {
-			$manifestPath = APPLICATION_PATH . '/../rev-manifest-' . 'staging'. '.json';
+			$manifestPath = APPLICATION_PATH . '/../rev-manifest-' . APPLICATION_ENV . '.json';
 			$this->_revManifest = json_decode(@file_get_contents($manifestPath), true);
 		}
 		return $this->_revManifest;
