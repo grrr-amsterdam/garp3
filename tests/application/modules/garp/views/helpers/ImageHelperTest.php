324682f209ea88666c45a9c67ad9007f6af8c1d3
Harmen Janssen <harmen@grrr.nl>
2016-03-29 22:22:57 +0200

    Modified and/or removed tests

--- tests/application/modules/garp/views/helpers/ImageHelperTest.php
+++ tests/application/modules/garp/views/helpers/ImageHelperTest.php
@@ -2,21 +2,21 @@
 /**
  * @group Image
  */
-class G_View_Helper_Image_Test extends PHPUnit_Framework_TestCase {
+class G_View_Helper_Image_Test extends Garp_Test_PHPUnit_TestCase {
 	public function test_source_url_should_not_be_empty() {
 		$imageHelper 	= $this->_getImageHelper();
 		$sourceUrl 		= $imageHelper->getSourceUrl('foo.png');
-		
+
 		$this->assertTrue(!empty($sourceUrl));
 	}
 
 
 	public function test_source_url_should_have_http_protocol() {
-		$imageHelper 	= $this->_getImageHelper();
-		$sourceUrl 		= $imageHelper->getSourceUrl('foo.png');
-		$first7chars	= substr($sourceUrl, 0, 7);
+		$imageHelper = $this->_getImageHelper();
+		$sourceUrl   = $imageHelper->getSourceUrl('foo.png');
+		$urlScheme   = parse_url($sourceUrl, PHP_URL_SCHEME);
 
-		$this->assertTrue($first7chars === 'http://');
+		$this->assertEquals('https', $urlScheme);
 	}
 
 
@@ -29,12 +29,32 @@ class G_View_Helper_Image_Test extends PHPUnit_Framework_TestCase {
 
 		$this->assertTrue($lastChars === $filename);
 	}
-	
-	
+
+
 	protected function _getImageHelper() {
         $bootstrap 		= Zend_Registry::get('application')->getBootstrap();
         $imageHelper 	= $bootstrap->getResource('View')->image();
 		return $imageHelper;
 	}
-	
+
+	public function setUp() {
+		parent::setUp();
+		$this->_helper->injectConfigValues(array(
+			'cdn' => array(
+				'type' => 'local',
+				'ssl' => true,
+				'path' => array(
+					'upload' => array(
+						'image' => '/images',
+						'document' => '/documents'
+					),
+					'static' => array(
+						'image' => '/static-images',
+						'document' => '/static-documents'
+					)
+				),
+				'extensions' => 'jpg,gif,png'
+			)
+		));
+	}
 }
