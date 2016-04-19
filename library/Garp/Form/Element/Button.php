42681908d03ea66250e2a6931fba2726e69bd55f
Harmen Janssen <harmen@grrr.nl>
2016-04-19 09:11:04 +0200

    Created Garp_Form_Element_Button to remove label

---
+++ library/Garp/Form/Element/Button.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Garp_Form_Element_Button
+ * Overwritten to remove label decorator
+ *
+ * @author       Harmen Janssen | grrr.nl
+ * @version      0.1.0
+ * @package      Garp_Form_Element
+ */
+class Garp_Form_Element_Button extends Zend_Form_Element_Button {
+
+		public function init() {
+			// Don't be silly. Buttons don't use labels.
+			$this->removeDecorator('Label');
+		}
+
+}
