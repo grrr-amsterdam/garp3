1d41599191d91bf6bd64c17ad0087f7bf20ee9d8
Harmen Janssen <harmen@grrr.nl>
2016-05-18 12:58:38 +0200

    Content Distributor respects read-only property

--- library/Garp/Content/Cdn/Distributor.php
+++ library/Garp/Content/Cdn/Distributor.php
@@ -16,19 +16,15 @@ class Garp_Content_Cdn_Distributor {
 	 */
 	const RELATIVE_BASEDIR_AFTER_APPLICATION_PATH = '/../public';
 
-
 	/**
 	 * System path without trailing slash.
 	 */
 	protected $_baseDir;
 
-
-
 	public function __construct($path = null) {
 		$this->_baseDir = realpath($path ?:
 			APPLICATION_PATH . self::RELATIVE_BASEDIR_AFTER_APPLICATION_PATH);
 	}
-
 
 	/**
 	 * Returns the list of available environments.
@@ -37,14 +33,12 @@ class Garp_Content_Cdn_Distributor {
 		return $this->_environments;
 	}
 
-
 	/**
 	 * @return String This instance's baseDir, without trailing slash.
 	 */
 	public function getBaseDir() {
 		return $this->_baseDir;
 	}
-
 
 	/**
 	 * Select assets to be distributed.
@@ -55,11 +49,8 @@ class Garp_Content_Cdn_Distributor {
 	 * @return 	Array 	$assetList 		A cumulative list of relative paths to the assets.
 	 */
 	public function select($filterString, $filterDate = null) {
-		$assetList = new Garp_Content_Cdn_AssetList($this->_baseDir, $filterString, $filterDate);
-
-		return $assetList;
+		return new Garp_Content_Cdn_AssetList($this->_baseDir, $filterString, $filterDate);
 	}
-
 
 	/**
 	 * @param String $env Name of the environment, f.i. 'development' or 'production'.
@@ -68,48 +59,48 @@ class Garp_Content_Cdn_Distributor {
 		$this->_validateEnvironment($env);
 
  		$ini = new Garp_Config_Ini(APPLICATION_PATH . '/configs/application.ini', $env);
-
-		if (
-			$assetCount &&
-			$ini->cdn->type === 's3'
-		) {
-			Garp_Cli::lineOut(ucfirst($env));
-			$progressBar = Garp_Cli_Ui_ProgressBar::getInstance();
-			$progressBar->init($assetCount);
-			$firstFilename = basename($assetList[0]);
-			$fileOrFiles = $this->_printFileOrFiles($assetCount);
-			$progressBar->display("Processing {$firstFilename}. {$assetCount} {$fileOrFiles} left.");
-
-			$s3 = new Garp_File_Storage_S3($ini->cdn, dirname(current($assetList)), true);
-
-			foreach ($assetList as $i => $asset) {
-				$s3->setPath(dirname($asset));
-				$fileData = file_get_contents($this->_baseDir . $asset);
-				$filename = basename($asset);
-				if ($s3->store($filename, $fileData, true, false)) {
-					$progressBar->advance();
-					$fileOrFiles = $this->_printFileOrFiles($assetCount - $progressBar->getProgress());
-					$progressBar->display("Processing {$filename}. %d {$fileOrFiles} left.");
-				} else {
-					$progressBar->displayError("Could not upload {$asset} to {$env}.");
-				}
-			}
-
-			if ($progressBar->getProgress() === $assetCount) {
-				$progressBar->display("√ Done");
-			}
-
-			echo "\n\n";
+		if ($ini->cdn->readonly) {
+			throw new Garp_File_Exception(Garp_File::EXCEPTION_CDN_READONLY);
 		}
-	}
 
+		if (!$assetCount || $ini->cdn->type !== 's3') {
+			return;
+		}
+
+		Garp_Cli::lineOut(ucfirst($env));
+		$progressBar = Garp_Cli_Ui_ProgressBar::getInstance();
+		$progressBar->init($assetCount);
+		$firstFilename = basename($assetList[0]);
+		$fileOrFiles = $this->_printFileOrFiles($assetCount);
+		$progressBar->display("Processing {$firstFilename}. {$assetCount} {$fileOrFiles} left.");
+
+		$s3 = new Garp_File_Storage_S3($ini->cdn, dirname(current($assetList)), true);
+
+		foreach ($assetList as $i => $asset) {
+			$s3->setPath(dirname($asset));
+			$fileData = file_get_contents($this->_baseDir . $asset);
+			$filename = basename($asset);
+			if ($s3->store($filename, $fileData, true, false)) {
+				$progressBar->advance();
+				$fileOrFiles = $this->_printFileOrFiles($assetCount - $progressBar->getProgress());
+				$progressBar->display("Processing {$filename}. %d {$fileOrFiles} left.");
+			} else {
+				$progressBar->displayError("Could not upload {$asset} to {$env}.");
+			}
+		}
+
+		if ($progressBar->getProgress() === $assetCount) {
+			$progressBar->display("√ Done");
+		}
+
+		echo "\n\n";
+	}
 
 	protected function _validateEnvironment($env) {
 		if (!in_array($env, $this->_environments)) {
 			throw new Exception("'{$env}' is not a valid environment. Try: " . implode(', ', $this->_environments));
 		}
 	}
-
 
 	protected function _printFileOrFiles($count) {
 		return 'file' . ($count == 1 ? '' : 's');
