986b23735cca07e9ec7d21eb578c96f3fc353115
Harmen Janssen <harmen@grrr.nl>
2016-08-05 10:11:22 +0200

    Refactored Garp_File_Unzipper
    It's no longer necessary to suppress the gzdecode error with @, since a
    more robust method of checking wether the file is zipped is implemented.

--- tests/library/Garp/File/UnzipperTest.php
+++ tests/library/Garp/File/UnzipperTest.php
@@ -1,11 +1,13 @@
 <?php
 /**
+ * @author Harmen Janssen <harmen@grrr.nl>
  * @group Zip
  */
 class Garp_File_UnzipperTest extends Garp_Test_PHPUnit_TestCase {
     const REGULAR_FILE = '/../tests/files/regular_file.txt';
     const ZIPPED_FILE = '/../tests/files/zipped_file.txt.gz';
     const DOUBLE_ZIPPED_FILE = '/../tests/files/double_zipped_file.txt.gz';
+    const ZIPPED_TOO_DEEP_FILE = '/../tests/files/zipped_too_deep_file.txt.gz';
 
     const ZIPPED_CONTENT = 'Hello I\'m zipped';
     const DOUBLE_ZIPPED_CONTENT = 'Hello I\'m double zipped';
@@ -27,4 +29,15 @@ class Garp_File_UnzipperTest extends Garp_Test_PHPUnit_TestCase {
         $unzipper = new Garp_File_Unzipper($doubleZippedContents);
         $this->assertEquals(self::DOUBLE_ZIPPED_CONTENT, $unzipper->getUnpacked());
     }
+
+    public function testShouldNeverExceedMaxRecursiveLevel() {
+        // This file is zipped recursively 20 times
+        $zippedTooDeepContents = file_get_contents(
+            GARP_APPLICATION_PATH . self::ZIPPED_TOO_DEEP_FILE
+        );
+        $unzipper = new Garp_File_Unzipper($zippedTooDeepContents);
+        // Unzipper should just return the original when it notices this
+        $this->assertEquals($zippedTooDeepContents, $unzipper->getUnpacked());
+    }
+
 }
