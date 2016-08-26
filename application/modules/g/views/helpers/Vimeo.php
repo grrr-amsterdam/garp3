b6b17e831febcf86724d8a25bc96d055655f4210
Harmen Janssen <harmen@grrr.nl>
2016-08-26 21:44:37 +0200

    PHPCS tweaks

--- application/modules/g/views/helpers/Vimeo.php
+++ application/modules/g/views/helpers/Vimeo.php
@@ -3,18 +3,18 @@
  * G_View_Helper_Vimeo
  * Helper for rendering embedded Vimeo players
  *
- * @author       Harmen Janssen | grrr.nl
- * @version      0.2.0
- * @package      G_View_Helper
+ * @package G_View_Helper
+ * @author  Harmen Janssen <harmen@grrr.nl>
  */
 class G_View_Helper_Vimeo extends Zend_View_Helper_HtmlElement {
     /**
      * Render a Vimeo player.
      * If no arguments are given, $this is returned
      * so there can be some chaining.
+     *
      * @param Garp_Db_Table_Row $vimeo A record from the `vimeo_videos` table
-     * @param Array $options Various rendering options
-     * @return Mixed
+     * @param array $options Various rendering options
+     * @return mixed
      */
     public function vimeo($vimeo = null, array $options = array()) {
         if (!func_num_args()) {
@@ -26,9 +26,10 @@ class G_View_Helper_Vimeo extends Zend_View_Helper_HtmlElement {
 
     /**
      * Render a Vimeo object tag.
+     *
      * @param Garp_Db_Table_Row $vimeo A record from the `vimeo_videos` table (or `media` view)
-     * @param Array $options Various rendering options
-     * @return Mixed
+     * @param array $options Various rendering options
+     * @return mixed
      */
     public function render($vimeo, array $options = array()) {
         $this->_setDefaultAttribs($options);
@@ -45,7 +46,7 @@ class G_View_Helper_Vimeo extends Zend_View_Helper_HtmlElement {
         $_attribs['frameborder'] = 0;
         $_attribs['src'] = $playerUrl;
 
-        $html = '<iframe'.$this->_htmlAttribs($_attribs).'></iframe>';
+        $html = '<iframe' . $this->_htmlAttribs($_attribs) . '></iframe>';
         return $html;
     }
 
@@ -54,35 +55,38 @@ class G_View_Helper_Vimeo extends Zend_View_Helper_HtmlElement {
         $playerurl = (is_string($vimeo) ? $vimeo :
             (isset($vimeo['player']) ? $vimeo['player'] : ''));
 
-        $playerurl .= '?'.http_build_query((array)$options);
+        $playerurl .= '?' . http_build_query((array)$options);
         return $playerurl;
     }
 
     /**
      * Normalize some configuration values.
-     * @param Array $options
-     * @return Array Modified options
+     *
+     * @param array $options
+     * @return array Modified options
      */
     protected function _setDefaultAttribs(&$options) {
-        $options = $options instanceof Garp_Util_Configuration ? $options : new Garp_Util_Configuration($options);
+        $options = $options instanceof Garp_Util_Configuration ?
+            $options : new Garp_Util_Configuration($options);
         $options
             ->setDefault('height', isset($options['width']) ? round($options['width']*0.55) : 264)
             ->setDefault('width', 480)
-            ->setDefault('attribs', array())
-        ;
+            ->setDefault('attribs', array());
     }
 
     /**
      * Normalize some query parameters
+     *
+     * @param array $options
+     * @return void
      * @see https://developers.google.com/youtube/player_parameters
-     * @param Array $options
      */
     protected function _setDefaultQueryParams(&$options) {
-        $options = $options instanceof Garp_Util_Configuration ? $options : new Garp_Util_Configuration($options);
+        $options = $options instanceof Garp_Util_Configuration
+            ? $options : new Garp_Util_Configuration($options);
         $options
             ->setDefault('portrait', 0)
             ->setDefault('title', 0)
-            ->setDefault('byline', 0)
-        ;
+            ->setDefault('byline', 0);
     }
 }
