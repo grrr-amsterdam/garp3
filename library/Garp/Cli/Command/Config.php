32d90ac0658008f51dcbb0abddad6238307dce36
Harmen Janssen <harmen@grrr.nl>
2016-09-02 00:05:27 +0200

    Refactored config loading in Garp_Application
    Together with `Garp_Cli_Command_Config`. It's now possible to read
    config data from another environment.
    
    This broke when the memcache stuff became required — it was no longer
    possible to call the command using `--e=staging` for instance.

--- library/Garp/Cli/Command/Config.php
+++ library/Garp/Cli/Command/Config.php
@@ -7,14 +7,25 @@
  * @author  Harmen Janssen <harmen@grrr.nl>
  */
 class Garp_Cli_Command_Config extends Garp_Cli_Command {
+
     public function get($args) {
         $key = $args[0];
-        $conf = Zend_Registry::get('config');
+        $env = isset($args[1]) ? $args[1] : APPLICATION_ENV;
+
+        $application = Zend_Registry::get('application');
+        $configFile = $application->getConfigFile();
+
+        $application = new Garp_Application($env, $configFile);
+        $conf = $application->getOptions();
+
         $bits = explode('.', $key);
-        while (isset($bits[0]) && isset($conf->{$bits[0]})) {
-            $conf = $conf->{$bits[0]};
+        while (isset($bits[0]) && isset($conf[$bits[0]])) {
+            $conf = $conf[$bits[0]];
             array_shift($bits);
         }
-        Garp_Cli::lineOut($conf);
+        Garp_Cli::lineOut(is_array($conf) ? print_r($conf, true) : $conf);
+
+        return true;
     }
+
 }
