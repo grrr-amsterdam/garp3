404b055901463bd807ebc386a63742bc2f2b5820
Harmen Janssen <harmen@grrr.nl>
2016-10-04 14:44:30 +0200

    Spawn timestampable fields as optional

--- library/Garp/Spawn/Behavior/Type/Timestampable.php
+++ library/Garp/Spawn/Behavior/Type/Timestampable.php
@@ -1,16 +1,22 @@
 <?php
 /**
- * @author David Spreekmeester | grrr.nl
+ * Garp_Spawn_Behavior_Type_Timestampable
+ * class description
+ *
+ * @package Garp_Spawn_Behavior_Type
+ * @author  David Spreekmeester <david@grrr.nl>
  */
 class Garp_Spawn_Behavior_Type_Timestampable extends Garp_Spawn_Behavior_Type_Abstract {
     protected $_fields = array(
         'created' => array(
             'type' => 'datetime',
-            'editable' => false
+            'editable' => false,
+            'required' => false
         ),
         'modified' => array(
             'type' => 'datetime',
-            'editable' => false
+            'editable' => false,
+            'required' => false
         )
     );
 }
