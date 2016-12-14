7bcb04b2d49b1bf70e9315b75343714f9e7d2cd7
Harmen Janssen <harmen@grrr.nl>
2016-12-14 19:07:22 +0100

    Added option allowImageExtensionsAsDocuments
    To allow image uploads as Document type

--- library/Garp/File.php
+++ library/Garp/File.php
@@ -1,15 +1,18 @@
 <?php
 /**
+ * Garp_File
  * Storage and retrieval of user uploads, either locally or on an external CDN.
- * @author David Spreekmeester | Grrr.nl
+ *
  * @package Garp
+ * @author  David Spreekmeester <david@grrr.nl>
  */
 class Garp_File {
-    const EXCEPTION_CDN_READONLY =
-        'You cannot upload or remove files here, CDN is configured read-only';
+    const EXCEPTION_CDN_READONLY
+        = 'You cannot upload or remove files here, CDN is configured read-only';
 
     /**
      * Constants that are used for filetypes
+     *
      * @var string
      */
     const TYPE_IMAGES = 'image';
@@ -31,8 +34,9 @@ class Garp_File {
     protected $_defaultUploadType = 'document';
 
     /**
-     * @var Garp_File_Storage_Protocol A Garp_File_Storage_Protocol compliant object,
-     *                                 such as Garp_File_Storage_S3.
+     * A Garp_File_Storage_Protocol compliant object such as Garp_File_Storage_S3.
+     *
+     * @var Garp_File_Storage_Protocol
      */
     protected $_storage;
 
@@ -42,20 +46,29 @@ class Garp_File {
 
     /**
      * Cache storage in a static property to save performance
-     * @var Array
+     *
+     * @var array
      */
     static protected $_cachedStorage = array();
 
     /**
-     * Store config from ini file statically to save performance when using a lot of Garp_File instances.
+     * Store config from ini file statically to save
+     * performance when using a lot of Garp_File instances.
+     *
      * @var Zend_Config
      */
     static protected $_config;
 
     /**
-    * @param String $uploadType Options: 'documents' or 'images'. Documents are all files besides images.
-    * @param Boolean $uploadOrStatic Options: 'upload' or 'static'. Whether this upload is a user upload, stored in the uploads directory, or a static file used in the site.
-    */
+     * Class constructor
+     *
+     * @param string $uploadType   Options: 'documents' or 'images'.
+     *                             Documents are all files besides images.
+     * @param bool $uploadOrStatic Options: 'upload' or 'static'. Whether this upload is a user
+     *                             upload, stored in the uploads directory,
+     *                             or a static file used in the site.
+     * @return void
+     */
     public function __construct($uploadType = null, $uploadOrStatic = null) {
         $this->validateUploadType($uploadType);
         $this->_validateUploadOrStatic($uploadOrStatic);
@@ -75,14 +88,22 @@ class Garp_File {
         return $this->_path;
     }
 
-    /** Make public methods of the Garp_File_Storage object available. */
+    /**
+     * Make public methods of the Garp_File_Storage object available.
+     *
+     * @param string $method
+     * @param array $args
+     * @return mixed
+     */
     public function __call($method, $args) {
         if (!$this->_storage) {
             $this->_initStorage($this->_getIni());
         }
         if (!method_exists($this->_storage, $method)) {
-            throw new BadMethodCallException('Call to undefined method ' .
-                get_class($this) . '::' . $method);
+            throw new BadMethodCallException(
+                'Call to undefined method ' .
+                get_class($this) . '::' . $method
+            );
         }
 
         return call_user_func_array(array($this->_storage, $method), $args);
@@ -121,13 +142,13 @@ class Garp_File {
 
     public static function formatFilename($filename) {
         if (strpos($filename, '/') !== false) {
-            throw new Exception(__FUNCTION__.'() is not for paths, please stick to filenames.');
+            throw new Exception(__FUNCTION__ . '() is not for paths, please stick to filenames.');
         }
         $filename = strtolower($filename);
         $plainFilename = preg_replace(
             array(
                 '/[_ ]/',
-                '/[^\da-zA-Z\.'.self::SEPERATOR.']/'
+                '/[^\da-zA-Z\.' . self::SEPERATOR . ']/'
             ),
             array(
                 self::SEPERATOR,
@@ -142,53 +163,65 @@ class Garp_File {
             ;
     }
 
-    /** Returns a filename with the next follow-up-number. F.i.: cookie.jpg -> cookie-2.jpg, cookie-15.jpg -> cookie-16.jpg */
+    /**
+     * Returns a filename with the next follow-up-number.
+     * F.i.: cookie.jpg -> cookie-2.jpg, cookie-15.jpg -> cookie-16.jpg
+     *
+     * @param string $filename
+     * @return string
+     */
     public static function getCumulativeFilename($filename) {
         $filename = self::formatFilename($filename);
 
         $filenameParts = explode('.', $filename);
         $ext = array_pop($filenameParts);
         $base = implode('.', $filenameParts);
-        $base = preg_match('/'.self::SEPERATOR.'\d+$/', $base) ?
+        $base = preg_match('/' . self::SEPERATOR . '\d+$/', $base) ?
             preg_replace_callback(
-                '/'.self::SEPERATOR.'(\d+)$/',
-                function($matches) {
-                    return Garp_File::SEPERATOR.++$matches[1];
+                '/' . self::SEPERATOR . '(\d+)$/',
+                function ($matches) {
+                    return Garp_File::SEPERATOR . ++$matches[1];
                 },
-                $base) :
-            $base.self::SEPERATOR.'2'
+                $base
+            ) :
+            $base . self::SEPERATOR . '2'
         ;
 
-        return $base.'.'.$ext;
+        return $base . '.' . $ext;
     }
 
     public function validateUploadType($uploadType) {
         if (!is_null($uploadType) && !in_array($uploadType, $this->_allowedTypes)) {
-            throw new Garp_File_Exception_InvalidType("'{$uploadType}' is not a valid " .
-                "upload type. Try: '".implode("' or '", $this->_allowedTypes)."'.");
+            throw new Garp_File_Exception_InvalidType(
+                "'{$uploadType}' is not a valid " .
+                "upload type. Try: '" . implode("' or '", $this->_allowedTypes) . "'."
+            );
         }
     }
 
     /**
-     * @param Boolean $returnNonImageExtensions Whether to return only non-image extensions, or all uploadable extensions.
-     * @return Array List of uploadable extensions
+     * @return array List of uploadable extensions
      */
     public function getAllowedExtensions() {
         $ini = $this->_getIni();
         $extensions = explode(',', $ini->cdn->extensions);
 
-        $imageFile = new Garp_Image_File($this->_uploadOrStatic);
-        $imageExtensions = $imageFile->getAllowedExtensions();
+        if (!$ini->cdn->allowImageExtensionsAsDocuments) {
+            $imageFile = new Garp_Image_File($this->_uploadOrStatic);
+            $imageExtensions = $imageFile->getAllowedExtensions();
 
-        $extensions = array_filter($extensions, function($element) use ($imageExtensions) {
-            return !in_array($element, $imageExtensions);
-        });
+            $extensions = array_filter(
+                $extensions, function ($element) use ($imageExtensions) {
+                    return !in_array($element, $imageExtensions);
+                }
+            );
+        }
 
         return $extensions;
     }
 
     /**
-     * @return Float Maximum upload filesize in megabytes.
+     * @return float Maximum upload filesize in megabytes.
      */
     public function getUploadMaxFilesize() {
         $val = null;
@@ -214,16 +247,17 @@ class Garp_File {
             $last = strtolower($val[strlen($val)-1]);
             switch($last) {
                 // The 'G' modifier is available since PHP 5.1.0
-                case 'g':
-                    $val *= 1024;
-                case 'm':
-                    $val *= 1024;
-                case 'k':
-                    $val *= 1024;
+            case 'g':
+                $val *= 1024;
+            case 'm':
+                $val *= 1024;
+            case 'k':
+                $val *= 1024;
             }
 
             return $val / 1024 / 1024;
-        } else throw new Exception("Could not retrieve the maximum filesize for uploads.");
+        }
+        throw new Exception("Could not retrieve the maximum filesize for uploads.");
     }
 
     public function clearContext() {
@@ -236,7 +270,12 @@ class Garp_File {
         $filenameParts = explode('.', $filename);
         if (count($filenameParts) >1) {
             return $filenameParts[count($filenameParts) -1];
-        } else throw new Exception("The provided filename does not have an extension. Please use the appropriate 3-character extension (such as .jpg, .png) after your filename.");
+        }
+        throw new Exception(
+            "The provided filename does not have an extension. " .
+            "Please use the appropriate 3-character extension (such as .jpg, .png) " .
+            "after your filename."
+        );
     }
 
     protected function _getPathFromConfig($uploadType) {
@@ -250,16 +289,10 @@ class Garp_File {
     protected function _validateConfig($ini) {
         if (isset($ini->cdn)) {
             foreach ($this->_requiredConfigParams as $param) {
-                if (
-                    (
-                        !isset($ini->cdn->{$param}) ||
-                        !$ini->cdn->{$param}
-                    ) &&
-                    !(
-                        //  Don't break on CLI
-                        $param === 'domain' &&
-                        defined('HTTP_HOST')
-                    )
+                if ((                    !isset($ini->cdn->{$param}) 
+                    || !$ini->cdn->{$param}) 
+                    && !(                    $param === 'domain' 
+                    && defined('HTTP_HOST'))
                 ) {
                     throw new Exception("'cdn.{$param}' was not set in application.ini.");
                 }
@@ -268,37 +301,52 @@ class Garp_File {
             if (in_array($configuredCdnType, $this->_storageTypes)) {
                 foreach ($this->_requiredConfigPaths as $uploadOrStatic) {
                     foreach ($this->_allowedTypes as $type) {
-                        if (
-                            !isset($ini->cdn->path->{$uploadOrStatic}->{$type}) ||
-                            !$ini->cdn->path->{$uploadOrStatic}->{$type}
+                        if (!isset($ini->cdn->path->{$uploadOrStatic}->{$type}) 
+                            || !$ini->cdn->path->{$uploadOrStatic}->{$type}
                         ) {
-                            throw new Exception("The required cdn.path.{$uploadOrStatic}.{$type} was not set in application.ini.");
+                            throw new Exception(
+                                "The required cdn.path.{$uploadOrStatic}.{$type} " .
+                                "was not set in application.ini."
+                            );
                         }
                     }
                 }
-            } else throw new Exception("'{$ini->cdn->type}' is not a valid CDN type. Try: ".implode(" or ", $this->_storageTypes).'.');
-        } else throw new Exception("The 'cdn' variable is not set in application.ini.");
+            }
+            throw new Exception(
+                "'{$ini->cdn->type}' is not a valid CDN type. Try: " .
+                implode(" or ", $this->_storageTypes) . '.'
+            );
+        }
+        throw new Exception("The 'cdn' variable is not set in application.ini.");
     }
 
     protected function _validateUploadOrStatic($uploadOrStatic) {
-        if ($uploadOrStatic !== 'upload' && $uploadOrStatic !== 'static' && !is_null($uploadOrStatic))
-            throw new Exception("The 'uploadOrStatic' variable should be either 'upload' or 'static' (dOh!) - so not '{$uploadOrStatic}'");
+        if ($uploadOrStatic !== 'upload'
+            && $uploadOrStatic !== 'static'
+            && !is_null($uploadOrStatic)
+        ) {
+            throw new Exception(
+                "The 'uploadOrStatic' variable should be either " .
+                "'upload' or 'static' (dOh!) - so not '{$uploadOrStatic}'"
+            );
+        }
     }
 
     protected function _initStorage($ini) {
-        if (!empty(self::$_cachedStorage[$ini->cdn->type][$this->_path]) &&
-            self::$_cachedStorage[$ini->cdn->type][$this->_path] instanceof Garp_File_Storage) {
+        if (!empty(self::$_cachedStorage[$ini->cdn->type][$this->_path]) 
+            && self::$_cachedStorage[$ini->cdn->type][$this->_path] instanceof Garp_File_Storage
+        ) {
             $this->_storage = self::$_cachedStorage[$ini->cdn->type][$this->_path];
         } else {
             switch ($ini->cdn->type) {
-                case 's3':
-                    $this->_storage = new Garp_File_Storage_S3($ini->cdn, $this->_path);
+            case 's3':
+                $this->_storage = new Garp_File_Storage_S3($ini->cdn, $this->_path);
                 break;
-                case 'local':
-                    $this->_storage = new Garp_File_Storage_Local($ini->cdn, $this->_path);
+            case 'local':
+                $this->_storage = new Garp_File_Storage_Local($ini->cdn, $this->_path);
                 break;
-                default:
-                    throw new Exception("The '{$ini->cdn->type}' protocol is not yet implemented.");
+            default:
+                throw new Exception("The '{$ini->cdn->type}' protocol is not yet implemented.");
             }
             self::$_cachedStorage[$ini->cdn->type][$this->_path] = $this->_storage;
         }
@@ -311,8 +359,10 @@ class Garp_File {
         $extension = $this->_getExtension($filename);
         $allowedExtensions = $this->getAllowedExtensions();
         if (!in_array(strtolower($extension), $allowedExtensions)) {
-            throw new Garp_File_Exception_InvalidType('The file type you\'re trying to upload is not allowed. Try: ' .
-                $this->_humanList($allowedExtensions, null, 'or'));
+            throw new Garp_File_Exception_InvalidType(
+                'The file type you\'re trying to upload is not allowed. Try: ' .
+                $this->_humanList($allowedExtensions, null, 'or')
+            );
         }
     }
 
@@ -325,20 +375,24 @@ class Garp_File {
     }
 
     /**
-     * @param Array $list Numeric Array of String elements,
-     * @param String $decorator Element decorator, f.i. a quote.
-     * @param String $lastItemSeperator Seperates the last item from the rest instead of a comma, for instance: 'and' or 'or'.
-     * @return String Listed elements, like "Snoop, Dre and Devin".
+     * @param array $list Numeric Array of String elements,
+     * @param string $decorator Element decorator, f.i. a quote.
+     * @param string $lastItemSeperator Seperates the last item from the rest
+     *                                  instead of a comma, for instance: 'and' or 'or'.
+     * @return string Listed elements, like "Snoop, Dre and Devin".
      */
     protected function _humanList(Array $list, $decorator = null, $lastItemSeperator = 'and') {
         $listCount = count($list);
         if ($listCount === 1) {
-            return $decorator.current($list).$decorator;
+            return $decorator . current($list) . $decorator;
         } elseif ($listCount === 2) {
-            return $decorator.implode($decorator." {$lastItemSeperator} ".$decorator, $list).$decorator;
+            return $decorator . implode($decorator . " {$lastItemSeperator} " . $decorator, $list) .
+                $decorator;
         } elseif ($listCount > 2) {
             $last = array_pop($list);
-            return $decorator.implode($decorator.", ".$decorator, $list).$decorator." {$lastItemSeperator} ".$decorator.$last.$decorator;
+            return $decorator . implode($decorator . ", " . $decorator, $list) . $decorator .
+                " {$lastItemSeperator} " . $decorator . $last . $decorator;
         }
     }
 }
+
--- library/Garp/File/Exception.php
+++ library/Garp/File/Exception.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Garp_File_Exception
+ * class description
+ *
+ * @package Garp_File
+ * @author  Harmen Janssen <harmen@grrr.nl>
+ */
 class Garp_File_Exception extends Exception {
-
 }
