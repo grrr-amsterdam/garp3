2f784b50764dee7f7dfc4f53bf8485f36181a588
Harmen Janssen <harmen@grrr.nl>
2018-06-12 15:37:26 +0200

    Wrap deletion of foreign key in try, catch

--- library/Garp/Spawn/MySql/ForeignKey.php
+++ library/Garp/Spawn/MySql/ForeignKey.php
@@ -1,43 +1,47 @@
 <?php
 /**
- * @author David Spreekmeester | grrr.nl
- * @package Garp
- * @subpackage Model
+ * @package Garp_Spawn_MySql
+ * @author  David Spreekmeester <david@grrr.nl>
  */
 class Garp_Spawn_MySql_ForeignKey extends Garp_Spawn_MySql_Key {
     public $name;
     public $localColumn;
     public $remoteTable;
     public $remoteColumn;
-    /** @var String $events MySql foreign key event string, f.i.: "ON DELETE SET NULL ON UPDATE SET NULL" */
+
+    /**
+     * @var string MySql foreign key event string, f.i.: "ON DELETE SET NULL ON UPDATE SET NULL"
+     */
     public $events;
 
 
     /**
-     * @param String $modelId ID of the model in which this foreign key is defined (local model).
-     * @param String $relName Name of the relation (remote table, or alias).
+     * @param  string $modelId ID of the model in which this foreign key is defined (local model).
+     * @param  string $relName Name of the relation (remote table, or alias).
+     * @return string
      */
     public static function generateForeignKeyName($modelId, $relName) {
-        return md5($modelId.$relName);
+        return md5($modelId . $relName);
     }
 
 
     /**
-     * @param String $fkName The foreign key name
-     * @param String $columnName The name of the relation column in the referencing table
-     * @param String $modelName The name of the table, usually the Model.
-     * @param String $relType The relation type, f.i. 'hasOne', 'belongsTo', 'hasAndBelongsToMany'.
+     * @param  string $fkName     The foreign key name
+     * @param  string $columnName The name of the relation column in the referencing table
+     * @param  string $modelName  The name of the table, usually the Model.
+     * @param  string $relType    The relation type, f.i. 'hasOne', 'belongsTo', 'hasAndBelongsToMany'.
+     * @return string
      */
     public static function renderSqlDefinition($fkName, $columnName, $modelName, $relType) {
         $lcModelName = strtolower($modelName);
 
         switch ($relType) {
-            case 'belongsTo':
-            case 'hasAndBelongsToMany':
-                $events = 'ON DELETE CASCADE ON UPDATE CASCADE';
+        case 'belongsTo':
+        case 'hasAndBelongsToMany':
+            $events = 'ON DELETE CASCADE ON UPDATE CASCADE';
             break;
-            default:
-                $events = 'ON DELETE SET NULL ON UPDATE CASCADE';
+        default:
+            $events = 'ON DELETE SET NULL ON UPDATE CASCADE';
         }
 
         return "  CONSTRAINT `{$fkName}` FOREIGN KEY (`{$columnName}`) REFERENCES `{$lcModelName}` (`id`) " . $events;
@@ -54,22 +58,34 @@ class Garp_Spawn_MySql_ForeignKey extends Garp_Spawn_MySql_Key {
     }
 
 
-    /** Delete a foreign key, if it exists. */
+    /**
+     * Delete a foreign key, if it exists.
+     *
+     * @param  string $tableName
+     * @param  Garp_Spawn_MySql_ForeignKey $key
+     * @return bool
+     */
     public static function delete($tableName, Garp_Spawn_MySql_ForeignKey $key) {
-        $tableName  = strtolower($tableName);
-        $adapter    = Zend_Db_Table::getDefaultAdapter();
+        $tableName = strtolower($tableName);
+        $adapter = Zend_Db_Table::getDefaultAdapter();
         $adapter->query("SET FOREIGN_KEY_CHECKS = 0;");
-        $success    = $adapter->query("ALTER TABLE `{$tableName}` DROP FOREIGN KEY `{$key->name}`;");
+        $success = $adapter->query("ALTER TABLE `{$tableName}` DROP FOREIGN KEY `{$key->name}`;");
         $adapter->query("SET FOREIGN_KEY_CHECKS = 1;");
         return $success;
     }
-    
-    
+
+
     public static function modify($tableName, Garp_Spawn_MySql_ForeignKey $key) {
         $tableName  = strtolower($tableName);
         $success    = false;
         $adapter    = Zend_Db_Table::getDefaultAdapter();
-        if (self::delete($tableName, $key)) {
+        try {
+            self::delete($tableName, $key);
+        } catch (Exception $e) {
+            if (strpos($e->getMessage(), 'Can\'t DROP') === false) {
+                throw $e;
+            }
+        } finally {
             $success = self::add($tableName, $key);
         }
         return $success;
