0b58a24af9d854fb24d7917ed5e58f015ff1c26d
Harmen Janssen <harmen@grrr.nl>
2018-07-18 08:39:21 +0200

    Allow attributes to be passed to AnyMarkup decorator

--- library/Garp/Form/Decorator/AnyMarkup.php
+++ library/Garp/Form/Decorator/AnyMarkup.php
@@ -1,25 +1,51 @@
 <?php
+use Garp\Functional as f;
+
 /**
  * Garp_Form_Decorator_AnyMarkup
- * Renders any markup. 
+ * Renders any markup.
+ *
  * Taken from @link http://www.zfsnippets.com/snippets/view/id/62/anymarkup-decorator.
- * @author chiborg | http://www.zfsnippets.com/users/view/id/326?PHPSESSID=83d3765c54898331cfb9a4439f245cd3
- * @version 1
- * @package Garp
- * @subpackage Form
+ * By use chiborg, http://www.zfsnippets.com/users/view/id/326?PHPSESSID=83d3765c54898331cfb9a4439f245cd3*
+ *
+ * @package Garp_Form_Decorator
+ * @author  Harmen Janssen <harmen@grrr.nl>
  */
 class Garp_Form_Decorator_AnyMarkup extends Zend_Form_Decorator_Abstract {
 
     public function render($content) {
         $placement = $this->getPlacement();
         $separator = $this->getSeparator();
-        switch ($placement) {
-            case self::PREPEND:
-                return $this->_options['markup'] . $separator . $content;
-            case self::APPEND:
-            default:
-                return $content . $separator . $this->_options['markup'];
+
+        $html = $this->_options['markup'];
+        $attribs = f\omit(['markup'], $this->_options);
+        if (count($attribs)) {
+            $html = $this->_addHtmlAttributes($html, $attribs);
         }
+
+        if ($placement === self::PREPEND) {
+            return $html . $separator . $content;
+        }
+        return $content . $separator . $html;
+    }
+
+    /**
+     * Take the html string, turn into a DOM fragment, add the attributes, and convert back.
+     * Bit experimental, requires being given valid and complete HTML (so no partial fragments).
+     *
+     * @param  string $html
+     * @param  array  $attribs
+     * @return string
+     */
+    protected function _addHtmlAttributes(string $html, array $attribs): string {
+        $dom = new DOMDocument();
+        $fragment = $dom->createDocumentFragment();
+        $fragment->appendXML($html);
+        $dom->appendChild($fragment);
+        foreach ($attribs as $attrib => $value) {
+            $dom->firstChild->setAttribute($attrib, $value);
+        }
+        return $dom->saveXML($dom->firstChild);
     }
 
 }
