77b30280a22bd414437993e1c4a95b83d3c0da6d
Martijn Gastkemper <martijn@grrr.nl>
2019-08-26 13:52:16 +0200

    Restore partial, snippet and __ in removed-functions.php

--- application/removed-functions.php
+++ application/removed-functions.php
@@ -14,6 +14,28 @@ function model(string $modelSuffix) {
 }
 
 /**
+ * Grab the rendered output of a snippet.
+ *
+ * @param string $identifier
+ * @return string
+ */
+function snippet(string $identifier): string {
+    $model = new Model_Snippet();
+    if ($model->isMultilingual()) {
+        $model = (new Garp_I18n_ModelFactory())->getModel('Snippet');
+    }
+    $snippet = $model->fetchByIdentifier($identifier);
+    if (!$snippet) {
+        return $identifier;
+    }
+    return strval(
+        $snippet->has_html
+            ? $snippet->html
+            : $snippet->text
+    );
+}
+
+/**
  * Quick access to the view.
  *
  * @return Zend_View_Abstract
@@ -24,6 +46,19 @@ function view(): Zend_View_Abstract {
     return Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')->view;
 }
 
+/**
+ * Render a partial.
+ *
+ * @param string $filename
+ * @param array $params
+ * @param string $module
+ * @return string
+ */
+function partial(string $filename, array $params = [], string $module = 'default'): string {
+    return Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')
+        ->view
+        ->partial($filename, $module, $params);
+}
 
 /**
  * Shortcut to logging messages.
@@ -44,6 +79,20 @@ function dump($file, $message, $priority = Zend_Log::INFO) {
     $logger = Garp_Log::factory($file);
     $message = is_array($message) ? print_r($message, true) : $message;
     $logger->log($message, $priority);
+}
+
+/**
+ * Translate text
+ *
+ * @param string $str
+ * @return string
+ */
+function __($str) {
+    if (Zend_Registry::isRegistered('Zend_Translate')) {
+        $translate = Zend_Registry::get('Zend_Translate');
+        return call_user_func_array(array($translate, '_'), func_get_args());
+    }
+    return $str;
 }
 
 /**
