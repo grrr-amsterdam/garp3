436f7c20abad461c13009a5fea554356e9d9d95d
Martijn Gastkemper <martijn@grrr.nl>
2019-12-05 08:14:06 +0100

    Let AssetUrl implement JsonSerializable interface

---
+++ tests/library/Garp/Util/AssetUrlTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class Garp_Util_AssetUrlTest
+ *
+ * @package Garp3
+ * @author Martijn Gastkemper <martijn@grrr.nl>
+ */
+class Garp_Util_AssetUrlTest extends Garp_Test_PHPUnit_TestCase {
+
+    public function setUp() {
+        parent::setUp();
+        $this->_helper->injectConfigValues(
+            array(
+                'cdn' => array(
+                    'baseUrl' => 'example.com'
+                )
+            )
+        );
+    }
+
+    public function toString() {
+        $url = new Garp_Util_AssetUrl();
+        $this->assertEquals('example.com', (string)$url);
+    }
+
+    public function testJsonSerialize() {
+        $url = new Garp_Util_AssetUrl();
+        $this->assertEquals('["example.com"]', json_encode([$url]));
+    }
+
+}
