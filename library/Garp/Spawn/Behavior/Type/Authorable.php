50cc70184607a81e7ba87abc8bb8991f66623d8d
Harmen Janssen <harmen@grrr.nl>
2020-03-04 15:49:42 +0100

    Remove hard-coded references to author_id

--- library/Garp/Spawn/Behavior/Type/Authorable.php
+++ library/Garp/Spawn/Behavior/Type/Authorable.php
@@ -1,15 +1,26 @@
 <?php
 /**
- * @author David Spreekmeester | grrr.nl
+ * @package Garp
+ * @author David Spreekmeester <david@grrr.nl>
  */
-class Garp_Spawn_Behavior_Type_Authorable extends Garp_Spawn_Behavior_Type_Abstract {   
+class Garp_Spawn_Behavior_Type_Authorable extends Garp_Spawn_Behavior_Type_Abstract {
     /**
      * @return  Bool    Whether this behavior needs to be registered with an observer
      *                  called in the PHP model's init() method
      */
     public function needsPhpModelObserver() {
         $model = $this->getModel();
-        
+
         return !$model->isTranslated();
     }
+
+    public function getColumns() {
+        $params = $this->getParams();
+        return [
+            $params['authorField'] ?? Garp_Model_Behavior_Authorable::_AUTHOR_COLUMN,
+            $params['modifierField'] ?? Garp_Model_Behavior_Authorable::_MODIFIER_COLUMN,
+        ];
+    }
+
 }
+
