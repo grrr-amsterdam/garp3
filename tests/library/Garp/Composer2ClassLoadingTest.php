e9ee438d4fb7b0fca72374bb55f1295c5b780388
Martijn Gastkemper <martijn@grrr.nl>
2021-10-11 17:07:10 +0200

    Load incompatible PSR-0 classes to classmap

---
+++ tests/library/Garp/Composer2ClassLoadingTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Test some files that used to be loaded by Composer 1 as PSR-0, but Composer 2 is more strict.
+ */
+class Garp_Composer2ClassLoadingTest extends Garp_Test_PHPUnit_TestCase {
+
+
+    public function testControllerClasses() {
+        $this->assertTrue(class_exists(G_RestController::class));
+    }
+
+    public function testViewHelpers() {
+        $this->assertTrue(class_exists(G_View_Helper_AssetUrl::class));
+        $this->assertTrue(class_exists(G_View_Helper_Html::class));
+    }
+
+}
